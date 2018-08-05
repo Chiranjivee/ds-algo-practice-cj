9b5461f23c837129cbe18fc226826ef5991e0121
Chiranjivee Thakur <KH2273@kitspl.com>
2018-08-06 01:25:19 +0530

    Implement rotation for linear array by factor k

---
+++ GeeksForGeeks/Arrays/Rotation/Program_to_rotate_an_array.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        int [] input = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(rotateArrayByK(input, 2)));
+    }
+
+    public static int [] rotateArrayByK(int [] array, int k) {
+        int rotateBy = k % array.length;
+        if (rotateBy == 0) {
+            return array;
+        }
+
+        int [] temp = new int[rotateBy];
+        for (int i = 0; i < rotateBy; i++) {
+            temp[i] = array[i];
+        }
+
+        int start = 0;
+        for (int i = rotateBy; i < array.length; i++) {
+            array[start] = array[i];
+            start++;
+        }
+
+        for (int i = 0; i < rotateBy; i++) {
+            array[start] = temp[i];
+            start++;
+        }
+
+        return array;
+    }
+}
