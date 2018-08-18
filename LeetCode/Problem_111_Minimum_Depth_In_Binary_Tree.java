b7194b18c34a01975bdc6d45e857d86b0e8add9f
Chiranjivee <cj@CJs-MacBook-Pro.local>
2018-08-18 22:57:42 +0530

    Add missing conditions for problem 111

--- LeetCode/Problem_111_Minimum_Depth_In_Binary_Tree.java
+++ LeetCode/Problem_111_Minimum_Depth_In_Binary_Tree.java
@@ -11,6 +11,8 @@ class Solution {
     public int minDepth(TreeNode root) {
         if (root == null) return 0;
         if (root.left == null && root.right == null) return 1;
+        if (root.left == null) return 1 + minDepth(root.right);
+        if (root.right == null) return 1 + minDepth(root.left);
         return 1 + Math.min(minDepth(root.left), minDepth(root.right));
     }
 }
