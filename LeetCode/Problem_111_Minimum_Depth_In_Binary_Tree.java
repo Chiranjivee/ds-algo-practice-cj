0d5c0276322f71251178f0beccad0648ff84c5b8
Chiranjivee <cj@CJs-MacBook-Pro.local>
2018-08-18 22:45:16 +0530

    Problem 111 Minimum Depth In Binary Tree

---
+++ LeetCode/Problem_111_Minimum_Depth_In_Binary_Tree.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+}
