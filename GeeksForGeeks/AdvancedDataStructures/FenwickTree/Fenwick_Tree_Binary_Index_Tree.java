307925df7d5f5a60a3fc5a57c667af0744c342f4
Chiranjivee <cjthehacker7@gmail.com>
2018-08-29 23:20:43 +0530

    Add update operation for fenwick tree

--- GeeksForGeeks/AdvancedDataStructures/FenwickTree/Fenwick_Tree_Binary_Index_Tree.java
+++ GeeksForGeeks/AdvancedDataStructures/FenwickTree/Fenwick_Tree_Binary_Index_Tree.java
@@ -3,7 +3,7 @@ class BinaryIndexTree {
     int treeSize;
 
     public BinaryIndexTree(int [] arr) {
-        treeSize = arr.length;
+        treeSize = arr.length + 1;
         tree = new int[treeSize];
     }
 
@@ -14,14 +14,44 @@ class BinaryIndexTree {
         }
     }
 
+    int getSum(int index)
+    {
+        if (index >= tree.length) {
+            System.out.println("Invalid input");
+            return -1;
+        }
+
+        int sum = 0;
+        index = index + 1;
+
+        while(index>0)
+        {
+            sum += tree[index];
+            index -= index & (-index);
+        }
+        return sum;
+    }
+
     public void updateBIT(int index, int arrLength, int element) {
-        
+        index = index + 1;
+        while (index <= arrLength) {
+            tree[index] += element;
+            index += index & (-index);
+        }
+    }
+
+    public void printFenwickTree() {
+        System.out.println("Printing fenwick tree.");
+        for (int i = 0; i < tree.length; i++) {
+            System.out.print(tree[i] + ", ");
+        }
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 3, -1, 9, -8, 17};
-
+        int[] arr = { 2, 3, -1, 9, -8, 17 };
         BinaryIndexTree fenwick = new BinaryIndexTree(arr);
-
+        fenwick.constructBIT(arr);
+        fenwick.printFenwickTree();
+        System.out.println("Sum from 0 to 3: " + fenwick.getSum(5));
     }
 }
