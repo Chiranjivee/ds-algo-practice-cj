339a44bdb48636bcc75c0ffe989aadb18d9f7bb4
Chiranjivee <cjthehacker7@gmail.com>
2018-09-07 00:04:31 +0530

    Binary Search Tree Implement delete functionality

--- GeeksForGeeks/AdvancedDataStructures/BalancedBST/BinarySearchTree.java
+++ GeeksForGeeks/AdvancedDataStructures/BalancedBST/BinarySearchTree.java
@@ -16,6 +16,7 @@ class Solution {
         public void insert(int data) {
             this.root = insert(data, this.getRoot());
         }
+
         public Node insert(int data, Node root) {
             if (root == null) {
                 return new Node(data);
@@ -28,11 +29,53 @@ class Solution {
         }
 
         public void delete(int data) {
-    
+            this.root = delete(data, this.getRoot());
+        }
+
+        public Node delete(int data, Node root) {
+            if (root == null) {
+                System.out.println("Key not available: " + data);
+                return null;
+            } 
+            if (root.getData() != data && data < root.getData()) {
+                root.setLeft(delete(data, root.getLeft()));
+            } else if (root.getData() != data && data > root.getData()) {
+                root.setRight(delete(data, root.getRight()));
+            }
+
+            if (root.getData() == data) {
+                if (root.getLeft() == null && root.getRight() == null) {
+                    root = null;
+                } else if (root.getLeft() == null && root.getRight() != null) {
+                    root = root.getRight();
+                } else if (root.getRight() == null && root.getLeft() != null) {
+                    root = root.getLeft();
+                } else {
+                    Node inOrderSuccessor = getInOrderSuccessor(root.getRight()); 
+                    if (inOrderSuccessor == null) {
+                        root = root.getLeft();
+                    } else {
+                        root.setData(inOrderSuccessor.getData());
+                        root.setRight(delete(inOrderSuccessor.getData(), root.getRight()));
+                    }
+                }
+            }
+
+            return root;
         }
 
         public void inOrder() {
             inOrder(this.root);
+        }
+
+        public Node getInOrderSuccessor(Node root) {
+            if (root == null) {
+                return null;
+            } if (root.getRight() == null && root.getLeft() == null) {
+                return root;
+            }
+            Node node = getInOrderSuccessor(root.getLeft());
+            return node == null ? root : node;
         }
 
         public void inOrder(Node root) {
@@ -59,6 +102,10 @@ class Solution {
             return this.data;
         }
 
+        public void setData(int data) {
+            this.data = data;
+        }
+
         public Node getLeft() {
             return this.left;
         }
@@ -78,9 +125,19 @@ class Solution {
 
     public void execute() {
         BinarySearchTree bst = new BinarySearchTree();
-        bst.insert(3);
-        bst.insert(2);
-        bst.insert(1);
+        System.out.println("Insert 3, 2, 1 in bst");
+        bst.insert(10);
+        bst.insert(15);
+        bst.insert(-4);
+        bst.insert(18);
+        bst.insert(-5);
+        bst.insert(0);
+        bst.insert(9);
+        bst.insert(-18);
+        bst.inOrder();
+
+        System.out.println("\nDelete 4");
+        bst.delete(4);
         bst.inOrder();
     }
 
