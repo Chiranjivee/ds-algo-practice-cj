c9e1e850a5b2124054cf707a0cf1f20cf0f02f74
Chiranjivee Thakur <cjthehacker7@gmail.com>
2018-09-10 18:14:51 +0530

    Fix bug in heapsort

--- GeeksForGeeks/Arrays/Sorting/HeapSort.java
+++ GeeksForGeeks/Arrays/Sorting/HeapSort.java
@@ -3,28 +3,28 @@ import java.util.Arrays;
 class Solution {
     public void heapSort(int [] arr) {
         int heapSize = arr.length;
-        heapify(arr, heapSize, 0);
-        for (int i = arr.length - 1; i > 0; i--) {
+        int start = (int) Math.floor(heapSize / 2) - 1;
+
+        for (int j = start; j >= 0; j--)
+            heapify(arr, heapSize, j);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
             swap(arr, 0, i);
             heapify(arr, --heapSize, 0);
         }
     }
 
-    public void heapify(int [] input, int heapSize, int heapRoot) {
-        int start = (int) Math.floor(heapSize / 2) - 1;
-        for (int j = start; j >= 0; j--) {
-            int leftIndex = getLeft(j);
-            int rightIndex = getRight(j);
-            int largest = j;
+    public void heapify(int [] input, int heapSize, int start) {
+        int leftIndex = getLeft(start);
+        int rightIndex = getRight(start);
+        int largest = start;
 
-            if (leftIndex < heapSize && input[leftIndex] > input[j]) largest = leftIndex;
-            if (rightIndex < heapSize && input[rightIndex] > input[j]) largest = rightIndex;
+        if (leftIndex < heapSize && input[leftIndex] > input[start]) largest = leftIndex;
+        if (rightIndex < heapSize && input[rightIndex] > input[largest]) largest = rightIndex;
+        if (largest == start) return;
 
-            if (largest != j) {
-                swap(input, largest, j);
-                heapify(input, heapSize, largest);
-            }
-        }
+        swap(input, largest, start);
+        heapify(input, heapSize, largest);
     }
 
     public int getLeft(int i) {
