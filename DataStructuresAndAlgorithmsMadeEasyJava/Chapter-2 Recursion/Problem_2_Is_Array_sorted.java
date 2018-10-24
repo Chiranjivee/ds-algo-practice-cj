ed59b0572ef2c2069463d77aaf6d5a8901eb9127
Chiranjivee <cjthehacker7@gmail.com>
2018-10-24 18:42:57 +0530

    Check is array sorted

---
+++ DataStructuresAndAlgorithmsMadeEasyJava/Chapter-2 Recursion/Problem_2_Is_Array_sorted.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        int [] test = { 1, 2, 0 };
+        System.out.println("Input array is: " + Arrays.toString(test));
+        System.out.println("Sorted: " + isArraySorted(test, test.length));  
+
+        int [] test2 = { 1, 2 };
+        System.out.println("Input array is: " + Arrays.toString(test2));
+        System.out.println("Sorted: " + isArraySorted(test2, test2.length));
+    }
+    public static boolean isArraySorted(int [] array, int index) {
+        if (index == 1) {
+            return true;
+        }
+
+        return array[index - 2] < array[index - 1] ? 
+            isArraySorted(array, index - 1) : false;
+    }
+}
