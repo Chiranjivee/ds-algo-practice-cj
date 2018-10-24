2fdfb44c4db1748cce9ca7ed600f4a514c9a9ece
Chiranjivee <cjthehacker7@gmail.com>
2018-10-24 18:30:23 +0530

    Add tower of hanoi

---
+++ DataStructuresAndAlgorithmsMadeEasyJava/Chapter-2 Recursion/Problem_1_Tower_of_Hanoi.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void main(String[] args) {
+        towerOfHanoi(3, 'A', 'B', 'C');
+    }
+
+    public static void towerOfHanoi(int n, char pegA, char pegB, char pegC) {
+
+        if (n == 1) {
+            System.out.println("Move from " + pegA + " to " + pegC);
+            return;
+        }
+
+        towerOfHanoi(n - 1, pegA, pegC, pegB);
+        System.out.println("Move from " + pegA + " to " + pegC);
+        towerOfHanoi(n - 1, pegB, pegA, pegC);
+    }
+}
