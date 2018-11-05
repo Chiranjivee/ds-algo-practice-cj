52d20e44f43e3ca7253133bdacdeb313f6d61ad8
Chiranjivee <cjthehacker7@gmail.com>
2018-11-05 15:22:01 +0530

    Add weights to adj matrix impl for graph

--- GeeksForGeeks/Graphs/GraphAdjMatrix.java
+++ GeeksForGeeks/Graphs/GraphAdjMatrix.java
@@ -3,30 +3,30 @@ class Graph {
     int maxVerticesCount;
     int verticesCount;
 
-    boolean [][] adjMatrix;
+    int [][] adjMatrix;
 
     Graph(int vertices) {
         this.maxVerticesCount = vertices;
         this.verticesCount = 0;
         this.vertices = new int[maxVerticesCount];
-        this.adjMatrix = new boolean[maxVerticesCount][maxVerticesCount];
+        this.adjMatrix = new int[maxVerticesCount][maxVerticesCount];
     }
 
     void addVertex(int v) {
         this.vertices[verticesCount++] = v;
     }
 
-    void addEdge(int u, int v) {
+    void addEdge(int u, int v, int weight) {
         if (u >= 0 && u < verticesCount
             && v >= 0 && v < verticesCount) {
-                adjMatrix[u][v] = true;
+                adjMatrix[u][v] = weight;
         }
     }
 
     void removeEdge(int u, int v) {
         if (u >= 0 && u < verticesCount
             && v >= 0 && v < verticesCount) {
-                adjMatrix[u][v] = false;
+                adjMatrix[u][v] = 0;
         }
     }
 
@@ -38,6 +38,17 @@ class Graph {
             System.out.println();
         }
     }
+
+    /*
+        (0) --5--> (1)
+                  / |
+                /   1
+               7    |
+             /      |
+           /        |
+          v         v
+        (2) --2--> (3)
+    */
     public static void main(String [] args) {
         Graph graph = new Graph(4);
         graph.addVertex(0);
@@ -45,10 +56,10 @@ class Graph {
         graph.addVertex(2);
         graph.addVertex(3);
 
-        graph.addEdge(0, 1);
-        graph.addEdge(2, 3);
-        graph.addEdge(1, 3);
-        graph.addEdge(1, 2);
+        graph.addEdge(0, 1, 5);
+        graph.addEdge(2, 3, 2);
+        graph.addEdge(1, 3, 1);
+        graph.addEdge(1, 2, 7);
 
         graph.showAdjMatrix();
     }
