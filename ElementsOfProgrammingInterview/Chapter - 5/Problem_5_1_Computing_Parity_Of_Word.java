044ff3c4b5256e9c5c9471e5f3fdc1339cc2971d
Chiranjivee <cjthehacker7@gmail.com>
2018-11-12 23:15:09 +0530

    Add log(n) implementation for computing parity

--- ElementsOfProgrammingInterview/Chapter - 5/Problem_5_1_Computing_Parity_Of_Word.java
+++ ElementsOfProgrammingInterview/Chapter - 5/Problem_5_1_Computing_Parity_Of_Word.java
@@ -34,19 +34,91 @@ class ComputeParity {
         }
         return result;
     }
+
+    /**
+     * Time Complexity: O(n/wordSize)
+     * Get parity for a 64 bit interger.
+     * The lookup cache
+     * Digit  Binary Parity
+     * 0      - 0000 - 0
+     * 1      - 0001 - 1
+     * 2      - 0010 - 1
+     * 3      - 0011 - 0
+     * 4      - 0100 - 1
+     * 5      - 0101 - 0
+     * 6      - 0110 - 0
+     * 7      - 0111 - 1
+     * 8      - 1000 - 1
+     * 9      - 1001 - 0
+     * 10     - 1010 - 0
+     * 11     - 1011 - 1
+     * 12     - 1100 - 0
+     * 13     - 1101 - 1
+     * 14     - 1110 - 1
+     * 15     - 1111 - 0
+     * @param x
+     * @return
+     */
+    public short getParityWithLookupCache(long x) {
+        short[] lookup = { 0, 1, 1, 0,
+                           1, 0, 0, 1,
+                           1, 0, 0, 1,
+                           0, 1, 1, 0 };
+        final int wordSize = 16;
+        final int mask = 0xFFFF;
+        return 
+            (short) 
+                (lookup[(int) (x >>> 3 * wordSize & mask)] ^ 
+                lookup[(int) (x >>> 2 * wordSize & mask)] ^ 
+                lookup[(int) (x >>> wordSize & mask)] ^ 
+                lookup[(int) (x & mask)]);
+    }
+    
+    /**
+     * Time Complexity: O(log(wordSize))
+     * We know that the XOR of all the bits of a number 
+     * is it's parity and also XOR is associative.
+     * extract the last bit as it is the result
+     * 
+     * 11001101 -> 1100 ^ 1101 = 0001 -> 00 ^ 01 = 01 -> 0 ^ 1
+     *          
+     */
+    public short getParityWithLogNComplexity(long x) {
+        x ^= x >>> 32;
+        x ^= x >>> 16;
+        x ^= x >>> 8;
+        x ^= x >>> 4;
+        x ^= x >>> 2;
+        x ^= x >>> 1;
+        return (short) (x & 0x1); 
+    }
     public static void main(String[] args) {
         int [] test = { 1, 2, 3, 4, 5 };
         ComputeParity parity = new ComputeParity();
+        System.out.println("With linear complexity:");
         for (int x : test) {
             System.out.println(
-                "Parity of " + test[x] + " is: " + 
+                "Parity of " + x + " is: " + 
                     parity.getParity(x));
         }
-
-        for (int x : test) {
+        System.out.println("With better-linear complexity:");
+        for (int y : test) {
             System.out.println(
-                "Parity of " + test[x] + " is: " + 
-                    parity.getParityWithBetterTimeComplexity(x));
+                "Parity of " + y + " is: " + 
+                    parity.getParityWithBetterTimeComplexity(y));
+        }
+        System.out.println("With O(n/l) complexity:");
+        for (int z : test) {
+            System.out.println(
+                "Parity of " + z + " is: " + 
+                    parity.getParityWithLookupCache(z));
+        }
+
+        System.out.println("With O(log(n)) complexity:");
+        for (int z : test) {
+            System.out.println(
+                "Parity of " + z + " is: " + 
+                    parity.getParityWithLogNComplexity(z));
         }
     }
 }
