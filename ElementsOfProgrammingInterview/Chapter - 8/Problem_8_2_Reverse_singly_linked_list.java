6236af9588edd780363f94596bccec4a6118bdd6
CJ <cjthehacker7@gmail.com>
2018-11-19 14:36:25 +0530

    Problem 8.2 reverse sublist

--- ElementsOfProgrammingInterview/Chapter - 8/Problem_8_2_Reverse_singly_linked_list.java
+++ ElementsOfProgrammingInterview/Chapter - 8/Problem_8_2_Reverse_singly_linked_list.java
@@ -6,8 +6,15 @@ class ReverseLinkedList {
         list.addAtEnd(2);
         list.addAtEnd(3);
         list.addAtEnd(4);
+        list.addAtEnd(5);
+        list.addAtEnd(6);
+        list.addAtEnd(7);
+        list.addAtEnd(8);
         list.print();
         list.reverse();
+        list.print();
+
+        list.reverseSublist(8, 1);
         list.print();
     }
 }
@@ -30,6 +37,61 @@ class LinkedList {
         ListNode temp = reverse(node.next);
         temp.next = node;
         return node;
+    }
+
+    public void reverseSublist(int start, int finish) {
+        if (head == null) {
+            return;
+        }
+
+        ListNode startPrev = null;
+        ListNode startNode = head;
+        int startIndex = 0;
+        int endIndex = 0;
+
+        while (startNode.data != start) {
+            startIndex++;
+            startPrev = startNode;
+            startNode = startNode.next;
+        }
+
+        ListNode finishPrev = null;
+        ListNode finishNode = head;
+        while (finishNode.data != finish) {
+            endIndex++;
+            finishPrev = finishNode;
+            finishNode = finishNode.next;
+        }
+
+        if (startNode == null || finishNode == null) {
+            return;
+        }
+
+        if (startIndex > endIndex) {
+            ListNode temp = startNode;
+            startNode = finishNode;
+            finishNode = temp;
+            startPrev = finishPrev;
+        }
+
+        ListNode finishNext = finishNode.next;
+        reverseSubList(startNode, finishNode, startPrev).next = finishNext;
+    }
+
+    private ListNode reverseSubList(ListNode startNode, ListNode finishNode, ListNode startPrev) {
+        if (startNode.next == finishNode.next) {
+            if (startPrev == null) {
+                this.head = startNode;
+            } else {
+                System.out.println("Start prev:" + startPrev.data);
+                startPrev.next = startNode;
+            }
+
+            return startNode;
+        }
+        ListNode temp = reverseSubList(startNode.next, finishNode, startPrev);
+        temp.next = startNode;
+        return startNode;
     }
 
     public void print() {
