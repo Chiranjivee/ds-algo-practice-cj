f105a19f70363e024dd704fe483813e7668b6f95
Chiranjivee <cjthehacker7@gmail.com>
2018-11-25 23:46:09 +0530

    Update string palindrome check

--- Practice/ValidStringPalindromicPermutation.java
+++ Practice/ValidStringPalindromicPermutation.java
@@ -4,24 +4,17 @@ class Solution {
         char [] arr = input.toCharArray();
         
         char xor = 0;
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             xor ^= arr[i];
         }
-        System.out.println(xor);
-        System.out.println("ArrayLeng:" + arr.length);
+
         if (arr.length % 2 == 0) {
             return xor == 0;
         }
-
-
-        // compute xor again skipping this char
+        
         char newxor = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == xor) {
-                System.out.println("Skiping: " + arr[i]);
-                continue;
-            }
-
+            if (arr[i] == xor) continue;
             newxor ^= arr[i];
         }
         return newxor == 0;
@@ -29,6 +22,7 @@ class Solution {
 
 
     public static void main(String [] args) {
-        System.out.println(Solution.isPermutationAValidPalindrome("aba"));
+        System.out.println(Solution.isPermutationAValidPalindrome("ababab"));
+        System.out.println(Solution.isPermutationAValidPalindrome("abab"));
     }
 }
