b9bae7832bf3a18b6816a3b1ad3d69e1689feb54
Chiranjivee <cjthehacker7@gmail.com>
2018-12-10 14:00:00 +0100

    10 Dec commit

--- Practice/BinarySearch.java
+++ Practice/BinarySearch.java
@@ -1,5 +1,5 @@
 class BinarySearch {
-    public static int searchUtil(int [] arr, int start, int end, int key) {
+    public static int searchUtil(int[] arr, int start, int end, int key) {
         if (start > end) {
             return -1;
         }
@@ -7,9 +7,7 @@ class BinarySearch {
         if (arr[mid] == key) {
             return mid;
         }
-        return arr[mid] > key ? 
-            searchUtil(arr, start, mid - 1, key) : 
-            searchUtil(arr, mid + 1, end, key);
+        return arr[mid] > key ? searchUtil(arr, start, mid - 1, key) : searchUtil(arr, mid + 1, end, key);
     }
 
     public static int search(int[] arr, int key) {
@@ -17,8 +15,7 @@ class BinarySearch {
     }
 
     public static void main(String[] args) {
-        int [] input = { 10, 20, 30, 40, 50 };
+        int[] input = { 10, 20, 30, 40, 50 };
         System.out.println(search(input, 50));
     }
 }
-
