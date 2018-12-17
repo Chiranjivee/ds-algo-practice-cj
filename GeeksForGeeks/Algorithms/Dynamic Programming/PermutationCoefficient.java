c6bdd5d5a14609be1c495fd1bb167583d9c2734c
Chiranjivee <cjthehacker7@gmail.com>
2018-12-17 21:51:46 +0530

    DP: Permutation coefficient

---
+++ GeeksForGeeks/Algorithms/Dynamic Programming/PermutationCoefficient.java
@@ -0,0 +1,51 @@
+class PermutationCoefficient {
+
+    /**
+     * recursive way
+     * @param n
+     * @param k
+     * @return
+     */
+    static int permutation(int n, int k) {
+        if (k == 0 || n == 0) return 1;
+        return k * permutation(n - 1, k - 1) + permutation(n - 1, k);
+    }
+
+
+    /**
+     * dynamic programming way
+     * @param n
+     * @param k
+     * @return
+     */
+    static int permutationMemoised(int n, int k) {
+        int [][] memo = new int[n + 2][k + 2];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= Math.min(i, k); j++) {
+                if (j == 0) {
+                    memo[i][j] = 1;    
+                } else {
+                    memo[i][j] = j * memo[i - 1][j - 1] + memo[i - 1][j];
+                }
+                memo[i][j + 1] = 0;   
+            }
+        }
+        
+        return memo[n][k];
+    }
+
+    static void printMemo(int [][] memo) {
+        System.out.println("Memo is: ");
+        for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo[0].length; j++) {
+                System.out.print(memo[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(permutation(10, 2));
+        System.out.println(permutationMemoised(10, 2));
+    }
+}
