9201fdc7d54e9a06a49af1588b966f19d88d46cd
Chiranjivee <cjthehacker7@gmail.com>
2019-01-05 22:16:02 +0530

    Update power code

--- CodingNinjas/CompetitiveProgramming/Recursion-basics/Power.java
+++ CodingNinjas/CompetitiveProgramming/Recursion-basics/Power.java
@@ -3,9 +3,10 @@ class Power {
         if (n == 0) return 1;
 		if (n == 1) return x;
 
-      	return (n % 2 == 0) ?
-           power(x * x, n / 2) :
-           x * power(x, n - 1);
+        return 
+            (n % 2 == 0) ?
+                power(x * x, n / 2) :
+                x * power(x, n - 1);
 	}
     public static void main(String[] args) {
         System.out.println("Power(2, 1): " + power(2, 1));
