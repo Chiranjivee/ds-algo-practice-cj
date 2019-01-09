8ee6c5c0b8ea8bd54b89eb088ae34929e01249ab
Chiranjivee <cjthehacker7@gmail.com>
2019-01-09 19:26:54 +0530

    Longest consequtie sequence

--- CodingNinjas/CompetitiveProgramming/Language-tools/WarmReception.java
+++ CodingNinjas/CompetitiveProgramming/Language-tools/WarmReception.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class WarmReception {
 	public static void main(String[] args) {
@@ -14,21 +14,62 @@ public class WarmReception {
           	end[j] = sc.nextInt();
         }
       
-     	int seats = 0;
-        int maxSeats = 0;
-      	for (int i = 0; i < 2360; i++) {
-          	
-          	for (int j = 0; j < n; j++) {
-              	if (i >= start[j] && i <= end[j]) {
-                  	seats++;
-                }
-              	if (maxSeats < seats) {
-                  	maxSeats = seats;
-                }
-            }
-          	seats = 0;
+      	List<Interval> intervals = new ArrayList<>();
+      	for (int i = 0; i < n; i++) {
+          	int diff = end[i] - start[i];
+          	if (diff > 0) intervals.add(new Interval(start[i], end[i]));
         }
-    
-      	System.out.println(maxSeats);
+      	if (intervals.size() == 0) {
+          	System.out.println(-1);
+          	return;
+        }
+		System.out.println(findMaxOverlapping(intervals));
+	}
+  
+  	public static int findMaxOverlapping(List<Interval> intervals) {
+      	int maxOverlapping = 0;
+      	for (int i = 0; i < intervals.size(); i++) {
+          	int currentInterval = i;
+          	for (int j = 0; j < intervals.size(); j++) {
+              	if (intervals.get(currentInterval).doesOverlap(intervals.get(j))) {
+                	intervals.get(currentInterval).overlap++;
+                }             	
+            }
+          
+          	if (intervals.get(maxOverlapping).overlap < intervals.get(currentInterval).overlap) {
+              	maxOverlapping = currentInterval;
+            }
+				}
+				
+				System.out.println("Overlapping interval: " + intervals.get(maxOverlapping).start + " " + intervals.get(maxOverlapping).end);
+      	return intervals.get(maxOverlapping).overlap;
+    }
+}
+
+class Interval {
+  
+  	int start;
+  	int end;
+  	int overlap;
+  
+  	public Interval(int start, int end) {
+      	this.start = start;
+      	this.end = end;
+      	this.overlap = 0;
+    }
+  
+  	public boolean doesOverlap(Interval i) {
+      	if (this.start >= i.start && this.start < i.end || 
+               this.end <= i.end && this.end > i.start ||
+               this.start == i.start && this.end == i.end) {
+          	return true;
+        }
+      	return false;
+    }
+  
+  	private boolean isBetweenThisInterval(int x) {
+      	if (start <= x && x <= end) {
+          	return true;
+        } return false;
     }
 }
