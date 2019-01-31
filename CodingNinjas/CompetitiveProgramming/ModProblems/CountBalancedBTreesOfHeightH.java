7029b9937c6f847dcb621552f90119da12df37f9
Chiranjivee <cjthehacker7@gmail.com>
2019-01-31 12:48:51 +0530

    Update cound balanced b trees of height h

--- CodingNinjas/CompetitiveProgramming/ModProblems/CountBalancedBTreesOfHeightH.java
+++ CodingNinjas/CompetitiveProgramming/ModProblems/CountBalancedBTreesOfHeightH.java
@@ -5,16 +5,13 @@ public class Solution {
         if (height == 0 || height == 1) {
             return 1;
         }
-
         int x = balancedTreesOfHeightH(height - 1);
         int y = balancedTreesOfHeightH(height - 2);
-        int xModM = x % m;
-        int yModM = y % m;
-        return
-        (
-            ((xModM * xModM) % m)
-            +
-            (((2 * xModM % m) * yModM) % m)
-        ) % m;
+  
+      	long ans1 = (long) x * x;
+      	int ans1Mod = (int) (ans1 % m);
+      	long ans2 = (long) x * y * 2;
+      	int ans2Mod = (int) (ans2 % m);
+      	return (ans1Mod + ans2Mod) % m;
     }
 }
