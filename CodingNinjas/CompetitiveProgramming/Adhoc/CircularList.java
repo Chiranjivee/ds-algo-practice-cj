436fb9daa9d57f286f011811bb5cd991d5109bee
Chiranjivee <cjthehacker7@gmail.com>
2019-02-04 20:44:04 +0530

    Circular list adhoc

---
+++ CodingNinjas/CompetitiveProgramming/Adhoc/CircularList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		while (t-- > 0) {
+          	int i = in.nextInt();
+          	int p = in.nextInt();
+          	System.out.println((i + p) % 12);
+        }
+	}
+}
