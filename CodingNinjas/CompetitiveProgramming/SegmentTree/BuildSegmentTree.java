22c34e9eafadcc0902ab4d2e3b3a31f219a68c89
CJ <cjthehacker7@gmail.com>
2019-02-19 21:03:18 +0530

    Segment tree query

--- CodingNinjas/CompetitiveProgramming/SegmentTree/BuildSegmentTree.java
+++ CodingNinjas/CompetitiveProgramming/SegmentTree/BuildSegmentTree.java
@@ -13,6 +13,8 @@ class BuildSegmentTree {
 
         System.out.println("Array: " + Arrays.toString(arr));
         System.out.println("Tree" + Arrays.toString(tree));
+
+        int ans = query(tree, 0, 4, 1, 2, 1);
     }
 
     public static void buildTree(int[] arr, int [] tree, int start, int end, int treeNodeIdx) {
@@ -27,6 +29,25 @@ class BuildSegmentTree {
         buildTree(arr, tree, mid + 1, end, 2 * treeNodeIdx + 1);
 
         tree[treeNodeIdx] = tree[2 * treeNodeIdx] + tree[2 * treeNodeIdx + 1];
+    }
+
+    public static int query(int [] tree, int start, int end, int left, int right, int treeNodeIdx) {
+        // Completely outside the given range.
+        if (start > right || end < left) {
+            return 0;
+        }
+
+        // Completely inside the given range.
+        if (start >= left && end <= right) {
+            return tree[treeNodeIdx];
+        }
+
+        // Partially inside and partially outside.
+        int mid = (start + end) / 2;
+        int option1 = query(tree, start, mid, left, right, 2 * treeNodeIdx);
+        int option2 = query(tree, mid + 1, end, left, right, 2 * treeNodeIdx + 1);
+
+        return option1 + option2;
     }
 
     public static void updateTree(int [] arr, int [] tree, int element, int index, int start, int end, int treeNodeIdx) {
