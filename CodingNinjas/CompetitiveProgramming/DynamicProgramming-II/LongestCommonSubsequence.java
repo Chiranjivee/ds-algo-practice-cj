11839e0231543f26b969e745dbdc825b5bf6a410
Chiranjivee <cjthehacker7@gmail.com>
2019-02-21 22:43:16 +0530

    [CodingNinjas][Dynamic Programming] LCS DP Bottom up

--- CodingNinjas/CompetitiveProgramming/DynamicProgramming-II/LongestCommonSubsequence.java
+++ CodingNinjas/CompetitiveProgramming/DynamicProgramming-II/LongestCommonSubsequence.java
@@ -5,6 +5,7 @@ class LongestCommonSubsequence {
 
         System.out.println(lcsRecursive(s1, s2));
         System.out.println(lcsMemoised(s1, s2));
+        System.out.println(lcsIterative(s1, s2));
     }
 
     public static int lcsRecursive(String s1, String s2) {
@@ -56,5 +57,31 @@ class LongestCommonSubsequence {
             dp[m][n] = Math.max(option1, option2);
             return dp[m][n];
         }
+    }
+
+    public static int lcsIterative(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int [][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int j = 0; j < dp[0].length; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 }
