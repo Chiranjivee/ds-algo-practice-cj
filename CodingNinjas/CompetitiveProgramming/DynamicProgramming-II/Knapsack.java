51e9b4c73cf91811f1f58af0e9af6ce547355c88
Chiranjivee <cjthehacker7@gmail.com>
2019-02-24 11:05:49 +0530

    [CodingNinjas][Dynamic Programming] KnapSack problem

---
+++ CodingNinjas/CompetitiveProgramming/DynamicProgramming-II/Knapsack.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Solution {
+
+	public static int knapsack(int[] weight,int value[],int maxWeight) {
+      	int n = weight.length;
+		int [][] dp = new int[n + 1][maxWeight + 1];
+      	for (int i = 0; i <= n; i++) {
+          	Arrays.fill(dp[i], -1);
+        }
+
+    	knapsack(weight, value, maxWeight, n, dp);  	   
+      	return dp[n][maxWeight];
+	}
+  
+  	public static int knapsack(int [] weight, int value[], int maxWeight, int n, int [][] dp) {
+      	if (n == 0 || maxWeight == 0) {
+          	return 0;
+        }
+      
+      	if (dp[n][maxWeight] != -1) return dp[n][maxWeight];
+      
+      	if (weight[n - 1] <= maxWeight) {
+          	int optionOne = value[n - 1] + knapsack(weight, value, maxWeight - weight[n - 1], n - 1, dp);
+          	int optionTwo = knapsack(weight, value, maxWeight, n - 1, dp);
+          	dp[n][maxWeight] = Math.max(optionOne, optionTwo);
+        } else {
+          	dp[n][maxWeight] = knapsack(weight, value, maxWeight, n - 1, dp);
+        }
+      	return dp[n][maxWeight];
+    }
+}
