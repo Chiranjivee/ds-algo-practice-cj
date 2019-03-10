6e8a806aa3fa3df5b17b2e4741506cb7f08fb3a3
Chiranjivee <cjthehacker7@gmail.com>
2019-03-10 20:17:56 +0530

    [CodingNinjas][SegmentTrees] Maximum Sum subarray

--- CodingNinjas/CompetitiveProgramming/SegmentTree/MaximumSumSubarray.java
+++ CodingNinjas/CompetitiveProgramming/SegmentTree/MaximumSumSubarray.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.BufferedReader; 
 import java.io.InputStreamReader; 
-import java.util.Scanner; 
 import java.util.StringTokenizer; 
 
 public class MaximumSumSubarray {
