043cdb2983c6400c6caff26d10be59380849036e
Chiranjivee <cjthehacker7@gmail.com>
2019-03-20 22:54:15 +0530

    Update kosaraju scc

--- CodingNinjas/CompetitiveProgramming/AdvancedGraphs/StronglyConnectedComponents.java
+++ CodingNinjas/CompetitiveProgramming/AdvancedGraphs/StronglyConnectedComponents.java
@@ -1,7 +1,43 @@
+import java.util.*;
+
 class StronglyConnectedComponents {
-
     public static void main (String[] args) {
+        int n = 10;
+        Graph graph = new Graph(n);
 
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 0);
+
+        graph.addEdge(2, 4);
+
+        graph.addEdge(4, 5);
+        graph.addEdge(5, 6);
+        graph.addEdge(6, 4);
+
+        graph.addEdge(7, 6);
+        graph.addEdge(7, 8);
+        graph.addEdge(8, 7);
+
+        graph.addEdge(8, 9);
+
+        boolean [] visited = new boolean[n];
+
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            graph.dfs(visited, i, st);
+        }
+
+        visited = new boolean[n];
+        graph.transpose();
+        while (!st.isEmpty()) {
+            int curr = st.pop();
+            if (!visited[curr]) {
+                graph.dfs(visited, curr);
+                System.out.println();
+            }
+        }
     }
 }
 
@@ -24,12 +60,12 @@ class Graph {
     public void transpose() {
         LinkedList<Integer>[] transposedList = new LinkedList[this.n];
         for (int i = 0; i < n; i++) {
-            this.transposedList[i] = new LinkedList<Integer>();
+            transposedList[i] = new LinkedList<Integer>();
         }
         for (int i = 0; i < this.adjList.length; i++) {
             LinkedList<Integer> neighbours = this.adjList[i];
             for (Integer e : neighbours) {
-                this.transposedList[e].add(i);
+                transposedList[e].add(i);
             }
         }
 
@@ -44,11 +80,25 @@ class Graph {
         visited[source] = true;
         for (Integer neighbour: this.adjList[source]) {
             if (!visited[neighbour]) {
-                dfs(visited, neighbour);
+                dfs(visited, neighbour, st);
             }
         }
 
         st.push(source);
+    }
+
+    public void dfs(boolean [] visited, int source) {
+        if (visited[source]) {
+            return;
+        }
+
+        visited[source] = true;
+        for (Integer neighbour: this.adjList[source]) {
+            if (!visited[neighbour]) {
+                dfs(visited, neighbour);
+            }
+        }
         System.out.print(source + " ");
+        // st.push(source);
     }
 }
