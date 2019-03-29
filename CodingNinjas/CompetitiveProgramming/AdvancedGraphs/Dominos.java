a27078840bd595e27e268cec0c5cfdfb6d29ad10
CJ <cjthehacker7@gmail.com>
2019-03-29 12:28:48 +0530

    [CodingNinjas][Advanced Graphs] Dominos

--- CodingNinjas/CompetitiveProgramming/AdvancedGraphs/Dominos.java
+++ CodingNinjas/CompetitiveProgramming/AdvancedGraphs/Dominos.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-public class Dominoes {
-    public static void main(String[] args) {
+public class Main {
+	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while (t-- > 0) {
@@ -14,24 +14,27 @@ public class Dominoes {
                 int v = sc.nextInt();
 
                 graph.addEdge(u - 1, v - 1);
-            }
-
+            }            
+            
             boolean[] visited = new boolean[n];
-            int[] depth = new int[n];
 
             Stack<Integer> st = new Stack<>();
-            int count = 0;
             for (int i = 0; i < n; i++) {
+                graph.dfs(visited, i, st);
+            }
+            
+            visited = new boolean[n];
+            int count = 0;
+            while (!st.isEmpty()) {
+                Integer i = st.pop();
                 if (!visited[i]) {
-                    depth[i] = graph.dfs(visited, i, depth);
+                    graph.dfs(visited, i);
                     count++;
                 }
             }
-
-            System.out.println(Arrays.toString(depth));
             System.out.println(count);
         }
-    }
+	}
 }
 
 class Graph {
@@ -50,19 +53,46 @@ class Graph {
         this.adjList[u].add(v);
     }
 
-    public int dfs(boolean[] visited, int source, int[] depth) {
-        if (visited[source]) {
-            return depth[source];
+    public void transpose() {
+        LinkedList<Integer>[] transposedList = new LinkedList[this.n];
+        for (int i = 0; i < n; i++) {
+            transposedList[i] = new LinkedList<Integer>();
         }
-
-        int currDepth = 1;
-        visited[source] = true;
-
-        for (Integer neighbour : this.adjList[source]) {
-            if (!visited[neighbour]) {
-                currDepth += dfs(visited, neighbour, depth);
+        for (int i = 0; i < this.adjList.length; i++) {
+            LinkedList<Integer> neighbours = this.adjList[i];
+            for (Integer e : neighbours) {
+                transposedList[e].add(i);
             }
         }
-        return currDepth;
+
+        this.adjList = transposedList;
+    }
+
+    public void dfs(boolean[] visited, int source, Stack<Integer> st) {
+        if (visited[source]) {
+            return;
+        }
+
+        visited[source] = true;
+        for (Integer neighbour : this.adjList[source]) {
+            if (!visited[neighbour]) {
+                dfs(visited, neighbour, st);
+            }
+        }
+
+        st.push(source);
+    }
+
+    public void dfs(boolean[] visited, int source) {
+        if (visited[source]) {
+            return;
+        }
+
+        visited[source] = true;
+        for (Integer neighbour : this.adjList[source]) {
+            if (!visited[neighbour]) {
+                dfs(visited, neighbour);
+            }
+        }
     }
 }
