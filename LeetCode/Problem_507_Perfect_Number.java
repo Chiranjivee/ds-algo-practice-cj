3984892072ce12ab2fb7e9f5032e832ec176e5f9
Chiranjivee <cjthehacker7@gmail.com>
2019-03-29 23:41:58 +0530

    [LeetCode][Problem 507] Perfect Number

---
+++ LeetCode/Problem_507_Perfect_Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num == 0 || num == 1) return false;
+        int sum = 1;
+        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                sum += (i + (num/i)) ;
+            }
+        }
+        return sum == num;
+    }
+}
