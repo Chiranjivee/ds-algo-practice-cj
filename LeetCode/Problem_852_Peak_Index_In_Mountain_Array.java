16972e05a6249b7f1ec65ed94a6cb350cdaa15b4
Chiranjivee <cjthehacker7@gmail.com>
2019-04-01 20:42:24 +0530

    LeetCode Problem 852 Peak index in mountain array

---
+++ LeetCode/Problem_852_Peak_Index_In_Mountain_Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (arr[mid - 1] > arr[mid] && arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else if (arr[mid - 1] < arr[mid] && arr[mid] < arr[mid + 1]) {
+                start = mid;
+            }
+        }
+        
+        return -1;
+    }
+}
