5fd84d0b9c1a0f9191c2550e28341fe815b09f72
Chiranjivee <cjthehacker7@gmail.com>
2019-04-04 23:02:50 +0530

    LeetCode Problem 461 Hamming Distance

---
+++ LeetCode/Problem_461_Hamming_Distance.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int count = 0;
+        while (xor > 0) {
+            count += xor & 1;
+            xor >>= 1;
+        }
+        return count;
+    }
+}
