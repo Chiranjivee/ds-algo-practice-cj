fc07724376edac923b8497150599aa05e8678122
Chiranjivee <cjthehacker7@gmail.com>
2019-04-04 21:21:27 +0530

    Problem 88 Merge Sorted arrays

---
+++ LeetCode/Problem_88_Merge_Sorted_Arrays.java
@@ -0,0 +1,42 @@
+/**
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ *
+ * Note:
+ * The number of elements initialized in nums1 and nums2 are m and n respectively.
+ * You may assume that nums1 has enough space (size that is greater or equal to m + n) 
+ * to hold additional elements from nums2.
+ */
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0) {
+            return;   
+        }
+
+        int num1Left = nums1.length - nums2.length - 1;
+        int num2Left = nums2.length - 1;
+        if (m == 0) {
+            for (int i = nums1.length - 1; i >= 0; i--) {            
+                nums1[i] = nums2[num2Left--];
+            }
+            return;
+        }
+        
+        
+        for (int i = nums1.length - 1; i >= 1; i--) {            
+            if (num1Left < 0) {
+                nums1[i] = nums2[num2Left--];
+            } else if (num2Left < 0)  {
+                nums1[i] = nums1[num1Left--];
+            } else {
+                nums1[i] = Math.max(nums1[num1Left], nums2[num2Left]);    
+                
+                if (nums1[num1Left] > nums2[num2Left]) {
+                    num1Left--;
+                } else {
+                    num2Left--;
+                }
+            }
+        }
+        nums1[0] = Math.min(nums1[0], nums2[0]);
+    }
+}
