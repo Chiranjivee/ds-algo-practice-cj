4ac9047f14d17cdbff8490599f8dc481c2ed2ddd
CJ <cjthehacker7@gmail.com>
2019-04-05 16:35:30 +0530

    Update counting string with comments

--- CodingNinjas/CompetitiveProgramming/DPWithBitMasking/CountingString.java
+++ CodingNinjas/CompetitiveProgramming/DPWithBitMasking/CountingString.java
@@ -7,10 +7,28 @@ public class CountingStrings {
 
 	    for(int i = 0; i < n; ++i) {
             int diff = 'Z' - s.charAt(i);
-    		ans = (ans + (greater + 1) * diff) % mod;
+
+            // compute how many values you can add to the current num
+            // including the current num and the numbers greater than the current num
+            // that's why adding one to greater
+            ans = (ans + (greater + 1) * diff) % mod;
+
+            // compute how many values are greater than the current value of same length
     		greater = ((greater * 26) % mod + diff) % mod;
     	}
         
         return ans;
 	}
 }
+
+/**
+ * greater = 8, ans = 8, Tere are 8 numbers graeter than 1
+ * 
+ * curr = 3
+ * diff = 6
+ * 
+ * 13
+ * 1 2 3 4 5 6 7 8 9
+ * 4, 5, 6, 7, 8, 9
+ * 
+ */
