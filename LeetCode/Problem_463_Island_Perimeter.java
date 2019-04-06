fd3db9e6734aefe2099fb768c97bc51d81c56c71
Chiranjivee <cjthehacker7@gmail.com>
2019-04-06 13:25:56 +0530

    [LeetCode] Problem 463 Island Perimeter

---
+++ LeetCode/Problem_463_Island_Perimeter.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1)
+                res += getPerimeter(grid, i, j);
+            }
+        }
+        return res;
+    }
+    
+    public int getPerimeter(int [][] grid, int i, int j) {
+        int res = 0;
+        
+        if (j + 1 >= grid[0].length || grid[i][j + 1] == 0) {
+            res++;
+        }
+
+        if (i + 1 >= grid.length || grid[i + 1][j] == 0) {
+            res++;
+        }
+        
+        if (j - 1 < 0 || grid[i][j - 1] == 0) {
+            res++;
+        }
+        
+        if (i - 1 < 0 || grid[i - 1][j] == 0) {
+            res++;
+        }
+        
+        return res;
+    }
+}
