a753d5add3fef0b446f3a5941a2c5a2a7b418934
Chiranjivee <cjthehacker7@gmail.com>
2019-04-10 21:26:38 +0530

    Refactor code

--- LeetCode/Problem_153_Find_Minimum_In_Rotated_Sorted_Array.java
+++ LeetCode/Problem_153_Find_Minimum_In_Rotated_Sorted_Array.java
@@ -2,7 +2,7 @@ class Solution {
     public int findMin(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
-        
+
         while (start < end) {
             int mid = (start + end) / 2;
             if (arr[mid] < arr[end]) {
--- LeetCode/Problem_463_Island_Perimeter.java
+++ LeetCode/Problem_463_Island_Perimeter.java
@@ -12,23 +12,10 @@ class Solution {
     
     public int getPerimeter(int [][] grid, int i, int j) {
         int res = 0;
-        
-        if (j + 1 >= grid[0].length || grid[i][j + 1] == 0) {
-            res++;
-        }
-
-        if (i + 1 >= grid.length || grid[i + 1][j] == 0) {
-            res++;
-        }
-        
-        if (j - 1 < 0 || grid[i][j - 1] == 0) {
-            res++;
-        }
-        
-        if (i - 1 < 0 || grid[i - 1][j] == 0) {
-            res++;
-        }
-        
+        if (j + 1 >= grid[0].length || grid[i][j + 1] == 0) res++;
+        if (i + 1 >= grid.length || grid[i + 1][j] == 0) res++;
+        if (j - 1 < 0 || grid[i][j - 1] == 0) res++;
+        if (i - 1 < 0 || grid[i - 1][j] == 0) res++;
         return res;
     }
 }
