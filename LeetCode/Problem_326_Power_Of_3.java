ec1ee99f6cb09ca20bebb3fb25098600b30c002e
Chiranjivee <cjthehacker7@gmail.com>
2019-04-12 22:48:07 +0530

    LeetCode Problem 326 Power of 3

--- LeetCode/Problem_326_Power_Of_3.java
+++ LeetCode/Problem_326_Power_Of_3.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n <= 0)
-            return false;
-
-        if (n % 3 == 0) {
-            if (n / 3 == 1)
-                return true;
-            else
-                return isPowerOfThree(n / 3);
-        } else if (n == 1)
-            return true;
+        if (n<=0) return false;
+        if (n == 1) return true;
+        if (n % 3 == 0) return isPowerOfThree(n/3);    
         return false;
+    }
+
+    public boolean isPowerOfThreeInConstantTime(int n) {
+        // 1162261467 is 3^19,  3^20 is bigger than int
+        // Basically modding with max power of 3 gives 0 for every power of 3
+        return (n > 0 && 1162261467 % n == 0);
     }
 }
