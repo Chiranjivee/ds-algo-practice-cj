36e98553aec403c703bbcdb108ef2724bcf678aa
Chiranjivee <cjthehacker7@gmail.com>
2019-04-14 19:21:09 +0530

    LeetCode Problem 73 Set Matrix Zeroes

---
+++ LeetCode/Problem_73_Set_Matrix_Zeroes.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean fr = false,fc = false;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    if(i == 0) fr = true;
+                    if(j == 0) fc = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) 
+            for (int j = 1; j < matrix[0].length; j++)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) 
+                    matrix[i][j] = 0;
+
+        if (fr)
+            for(int j = 0; j < matrix[0].length; j++)
+                matrix[0][j] = 0;
+
+        if (fc)
+            for(int i = 0; i < matrix.length; i++)
+                matrix[i][0] = 0;
+    }
+}
