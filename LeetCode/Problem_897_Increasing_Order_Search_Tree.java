0d193906be46fb3e5a8a24c905293b08b097c890
Chiranjivee <cjthehacker7@gmail.com>
2019-04-15 21:32:56 +0530

    Problem 897 Increasing order search tree

---
+++ LeetCode/Problem_897_Increasing_Order_Search_Tree.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+        return increasingBST(root, null);
+    }
+
+    public TreeNode increasingBST(TreeNode root, TreeNode tail) {
+        if (root == null) return tail;
+        TreeNode res = increasingBST(root.left, root);
+        root.left = null;
+        root.right = increasingBST(root.right, tail);
+        return res;
+    }
+}
