cea114aae9948f5f81bfe91ad9f969cd851a9383
Chiranjivee <cjthehacker7@gmail.com>
2019-04-20 15:43:42 +0530

    LeetCode PRoblem 109 Convert sorted list to bst

---
+++ LeetCode/Problem_109_Convert_Sorted_List_To_BST.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        // find mid 
+        return sortedListToBST(head, null);
+    }
+    
+    public TreeNode sortedListToBST(ListNode head, ListNode tail) {
+        if (head == tail) return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != tail && fast.next != tail) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        TreeNode root = new TreeNode(slow.val);
+        root.left = sortedListToBST(head, slow);
+        root.right = sortedListToBST(slow.next, tail);
+
+        return root; 
+    }
+}
