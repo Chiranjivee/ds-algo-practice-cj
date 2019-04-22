d627825a3ed5912b7298473bf45fee886af539b3
CJ <cjthehacker7@gmail.com>
2019-04-22 16:14:45 +0530

    [CodingNinjas][Tries] MaximumXOR Subarray

--- CodingNinjas/CompetitiveProgramming/Tries/MaximumXORSubarray.java
+++ CodingNinjas/CompetitiveProgramming/Tries/MaximumXORSubarray.java
@@ -1,72 +1,85 @@
-class MaximumXORSubarray {
+import java.util.Scanner;
+
+public class Main {
+    public static class TrieNode {
+        TrieNode left, right;
+        int value = 0;
+    }
+
+    public static class Trie {
+        TrieNode root;
+
+        public Trie() {
+            this.root = new TrieNode();
+        }
+
+        void insert(int val) {
+            TrieNode curr = root;
+            for (int i = 31; i >= 0; i--) {
+                int bit = (val >> i) & 1;
+                if (bit == 0) {
+                    if (curr.left == null) {
+                        curr.left = new TrieNode();
+                    }
+                    curr = curr.left;
+                } else {
+                    if (curr.right == null) {
+                        curr.right = new TrieNode();
+                    }
+
+                    curr = curr.right;
+                }
+            }
+            curr.value = val;
+        }
+
+        int query(int xor) {
+            TrieNode curr = root;
+            for (int i = 31; i >= 0; i--) {
+                int bit = (xor >> i) & 1;
+                if (bit == 0) {
+                    if (curr.right != null) {
+                        curr = curr.right;
+                    } else if (curr.left != null) {
+                        curr = curr.left;
+                    }
+                } else {
+                    if (curr.left != null) {
+                        curr = curr.left;
+
+                    } else if (curr.right != null) {
+                        curr = curr.right;
+                    }
+                }
+            }
+
+            return xor ^ curr.value;
+        }
+    }
+
     public static void main(String[] args) {
-        int [] arr = new int[] {1, 2, 3, 4};
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
 
         int maxXor = Integer.MIN_VALUE;
 
         Trie trie = new Trie();
         int xor = 0;
+        trie.insert(xor);
+
         for (int i = 0; i < arr.length; i++) {
             int val = arr[i];
+
             TrieNode curr = trie.root;
             xor = xor ^ arr[i];
             trie.insert(xor);
-
-            // for current element find max xor
-            int currXor = 0;
-            for (int j = 31; j >= 0; j--) {
-                int b = (val >> j) & 1;
-                if (b == 0) {
-                    if (curr.right != null) {
-                        currXor += (1 << j);
-                        curr = curr.right;
-                    } else {
-                        curr = curr.left;
-                    }
-                } else {
-                    if (curr.left != null) {
-                        currXor += (1 << j);
-                        curr = curr.left;
-                    } else {
-                        curr = curr.right;
-                    }
-                }
-            }
-
-            maxXor = Math.max(currXor, maxXor);
+            maxXor = Math.max(maxXor, trie.query(xor));
         }
 
         System.out.println(maxXor);
-    }
-}
-
-class TrieNode { 
-    TrieNode left;
-    TrieNode right;
-}
-
-class Trie {
-    TrieNode root;
-
-    public Trie() {
-        this.root = new TrieNode();
-    }
-    void insert(int val) {
-        TrieNode curr = root;
-        for (int i = 31; i >= 0; i--) {
-            int bit = (val >> i) & 1;
-            if (bit == 0) {
-                if (curr.left == null) {
-                    curr.left = new TrieNode();    
-                } 
-                curr = curr.left;
-            } else {
-                if (curr.right == null) {
-                    curr.right= new TrieNode();
-                }
-
-                curr = curr.right;                
-            }
-        }
     }
 }
