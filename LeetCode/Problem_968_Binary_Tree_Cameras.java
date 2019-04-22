406004fc647edb2b221994c94022ada19fba75a1
Chiranjivee <cjthehacker7@gmail.com>
2019-04-22 21:23:14 +0530

    LeetCode Problem 968 Binary Tree Cameras

---
+++ LeetCode/Problem_968_Binary_Tree_Cameras.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int res = 0;
+    public int minCameraCover(TreeNode root) {
+        return (dfs(root) < 1 ? 1 : 0) + res;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) return 2;
+        int left = dfs(root.left), right = dfs(root.right);
+        if (left == 0 || right == 0) {
+            res++;
+            return 1;
+        }
+        return left == 1 || right == 1 ? 2 : 0;
+    }
+}
