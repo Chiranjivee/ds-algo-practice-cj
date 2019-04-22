58bed8291168b575c4fadfdfb6e63aa49dc15fee
Chiranjivee <cjthehacker7@gmail.com>
2019-04-22 22:14:25 +0530

    Problem 513 Find Bottom left value of tree

---
+++ LeetCode/Problem_513_Find_Bottom_Left_Value_Of_Tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int res = 0;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                if (i == 0) {
+                    res = curr.val;
+                }
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+        }
+        
+        return res;
+    }
+}
