7a5326ec4f4060be158914e27ec451985d5d3d00
Chiranjivee <cjthehacker7@gmail.com>
2019-04-22 21:36:01 +0530

    LeetCode Probblem 655 Print binary tree

---
+++ LeetCode/Problem_655_Print_Binary_Tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public List<List<String>> printTree(TreeNode root) {
+    List<List<String>> res = new LinkedList<>();
+        int height = root == null ? 1 : getHeight(root);
+        int rows = height, columns = (int) (Math.pow(2, height) - 1);
+        List<String> row = new ArrayList<>();
+        for(int i = 0; i < columns; i++)  row.add("");
+        for(int i = 0; i < rows; i++)  res.add(new ArrayList<>(row));
+        populateRes(root, res, 0, rows, 0, columns - 1);
+        return res;
+    }
+
+    public void populateRes(TreeNode root, List<List<String>> res, int row, int totalRows, int i, int j) {
+        if (row == totalRows || root == null) return;
+        int mid = (i + j) / 2;
+        res.get(row).set(mid, Integer.toString(root.val));
+        populateRes(root.left, res, row + 1, totalRows, i, mid - 1);
+        populateRes(root.right, res, row + 1, totalRows, mid + 1, j);
+    }
+
+    public int getHeight(TreeNode root) {
+         if (root == null) return 0;
+         return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    }
+}
