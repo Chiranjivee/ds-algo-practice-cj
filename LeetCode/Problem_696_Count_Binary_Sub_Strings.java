191878bb40b6bff4c25e51f3d2a19271de378a53
CJ <cjthehacker7@gmail.com>
2019-04-25 13:05:14 +0530

    LeetCode Problem 696 Count Binary Sub Strings

---
+++ LeetCode/Problem_696_Count_Binary_Sub_Strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int res = 0;
+        int curr = 1;
+        int pre = 0;
+        
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) == s.charAt(i)) {
+                curr++;
+            } else {
+                res += Math.min(pre, curr);
+                pre = curr;
+                curr = 1;
+            }
+        }
+        
+        return res + Math.min(pre, curr);
+    }
+}
