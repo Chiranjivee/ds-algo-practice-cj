65d86dd4ec0d9ab1790aa21538a098c1a2774102
Chiranjivee <cjthehacker7@gmail.com>
2019-04-26 08:38:26 +0530

    LeetCode Problem 219 Contains Duplicate II

---
+++ LeetCode/Problem_219_Contains_Duplicate_II.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            
+            map.put(nums[i], i);
+        }
+        
+        return false;
+    }
+}
