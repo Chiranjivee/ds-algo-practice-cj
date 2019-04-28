617a6ad6662b63983989343b69c28d9108d86d48
Chiranjivee <cjthehacker7@gmail.com>
2019-04-28 14:53:33 +0530

    LeetCode Problem830 Position of large groups

---
+++ LeetCode/Problem_830_Position_Of_Large_Groups.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<List<Integer>> largeGroupPositions(String S) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0, j = 0; i < S.length(); i = j) {
+            while (j < S.length() && S.charAt(j) == S.charAt(i)) ++j;
+            if (j - i >= 3)
+                res.add(Arrays.asList(i, j - 1));
+        }
+        return res;
+    }
+}
