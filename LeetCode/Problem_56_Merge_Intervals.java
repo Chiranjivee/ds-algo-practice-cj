79da3cb70b6f8640433c862b781c8af1391f2b2a
CJ <cjthehacker7@gmail.com>
2019-04-29 17:02:30 +0530

    LeetCode Problem 56 Merge Intervals

---
+++ LeetCode/Problem_56_Merge_Intervals.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> ret = new ArrayList<>();
+        int[] prev = null;
+        for (int[] inter : intervals) {
+            //if prev is null or curr.start > prev.end, add the interval
+            if (prev==null || inter[0] > prev[1]) {
+                ret.add(inter);
+                prev = inter;
+            } else if (inter[1] > prev[1]) {
+                // curr.end > prev.end, modify the element already in list
+                prev[1] = inter[1];
+            }
+        }
+        return ret.toArray(new int[ret.size()][2]);
+    }
+}
