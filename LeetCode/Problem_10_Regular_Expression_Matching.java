fe4ee3b47c8abfce839a817b242294530b93c20d
CJ <cjthehacker7@gmail.com>
2019-04-29 16:51:23 +0530

    LeeTCode Problem 10 Regular expression matching

---
+++ LeetCode/Problem_10_Regular_Expression_Matching.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (p.isEmpty()) return s.isEmpty();
+        
+        boolean firstMatch = (!s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            return (isMatch(s, p.substring(2)) ||
+                    (firstMatch && isMatch(s.substring(1), p)));
+        } else {
+            return firstMatch && isMatch(s.substring(1), p.substring(1));
+        }
+    }
+}
