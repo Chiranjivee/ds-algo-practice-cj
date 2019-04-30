0244a687e98749d839726acb1223d742d3fdc587
Chiranjivee <cjthehacker7@gmail.com>
2019-04-30 22:36:14 +0530

    LeetCode Problem 599 Minimum Index sum of two sublists

---
+++ LeetCode/Problem_599_Minimum_Index_Sum_Of_Two_Lists.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String, Integer> map = new HashMap<>();
+        List<String> res = new LinkedList<>();
+
+        int minSum = Integer.MAX_VALUE;
+        for (int i=0;i<list1.length;i++) map.put(list1[i], i);
+
+        for (int i=0;i<list2.length;i++) {
+            Integer j = map.get(list2[i]);
+            if (j != null && i + j <= minSum) {
+                if (i + j < minSum) { 
+                    res.clear();
+                    minSum = i+j;
+                }
+
+                res.add(list2[i]);
+            }
+        }
+
+        return res.toArray(new String[res.size()]);
+    }
+}
