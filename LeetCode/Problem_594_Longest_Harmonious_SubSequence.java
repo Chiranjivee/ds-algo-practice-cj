95b5909c296b6fbffc9966d8e4b8490d7eaf71ef
Chiranjivee <cjthehacker7@gmail.com>
2019-05-01 20:59:43 +0530

    Problem 594 Longest harmonious subsequence

---
+++ LeetCode/Problem_594_Longest_Harmonious_SubSequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int key: map.keySet()) {
+            if (map.containsKey(key + 1))
+                res = Math.max(res, map.get(key) + map.get(key + 1));
+        }
+        return res;
+    }
+}
