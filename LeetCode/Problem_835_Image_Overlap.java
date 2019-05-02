91119ad8b6471eb7c882d148f28adbe4a70b298b
Chiranjivee <cjthehacker7@gmail.com>
2019-05-02 20:29:25 +0530

    Problem 835 Image Overlap

---
+++ LeetCode/Problem_835_Image_Overlap.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int largestOverlap(int[][] A, int[][] B) {
+        int N = A.length;
+        int[][] count = new int[2*N+1][2*N+1];
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j < N; ++j)
+                if (A[i][j] == 1)
+                    for (int i2 = 0; i2 < N; ++i2)
+                        for (int j2 = 0; j2 < N; ++j2)
+                            if (B[i2][j2] == 1)
+                                count[i-i2 +N][j-j2 +N] += 1;
+
+        int ans = 0;
+        for (int[] row: count)
+            for (int v: row)
+                ans = Math.max(ans, v);
+        return ans;
+    }
+}
