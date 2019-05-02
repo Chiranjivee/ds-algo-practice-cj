0636e4ee0d3abcada4399c34f79378d98b7f19cb
Chiranjivee <cjthehacker7@gmail.com>
2019-05-02 19:58:08 +0530

    Problem 833 Find and replace in a string

---
+++ LeetCode/Problem_833_Find_And_Replace_In_A_String.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String findReplaceString(String S, int[] indexes, String[] sources, String[] targets) {
+        int N = S.length();
+        int[] match = new int[N];
+        Arrays.fill(match, -1);
+
+        for (int i = 0; i < indexes.length; ++i) {
+            int ix = indexes[i];
+            if (S.substring(ix, ix + sources[i].length()).equals(sources[i]))
+                match[ix] = i;
+        }
+
+        StringBuilder ans = new StringBuilder();
+        int ix = 0;
+        while (ix < N) {
+            if (match[ix] >= 0) {
+                ans.append(targets[match[ix]]);
+                ix += sources[match[ix]].length();
+            } else {
+                ans.append(S.charAt(ix++));
+            }
+        }
+        return ans.toString();
+    }
+}
