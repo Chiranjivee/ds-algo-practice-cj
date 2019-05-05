8923c1024bbbe1bf5ec6e40208b80c14b74b95d5
Chiranjivee <cjthehacker7@gmail.com>
2019-05-05 21:40:27 +0530

    Problem 981 Time based key value map

---
+++ LeetCode/Problem_981_Time_Based_Key_Value_Map.java
@@ -0,0 +1,38 @@
+import javafx.util.Pair;
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
+class TimeMap {
+
+    /** Initialize your data structure here. */
+    Map<String, List<Pair<Integer, String>>> M;
+    public TimeMap() {
+        M = new HashMap();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if (!M.containsKey(key))
+            M.put(key, new ArrayList<Pair<Integer, String>>());
+
+        M.get(key).add(new Pair(timestamp, value));
+    }
+    
+    public String get(String key, int timestamp) {
+        if (!M.containsKey(key)) return "";
+
+        List<Pair<Integer, String>> A = M.get(key);
+        int i = Collections.binarySearch(A, new Pair<Integer, String>(timestamp, "{"),
+                (a, b) -> Integer.compare(a.getKey(), b.getKey()));
+
+        if (i >= 0)
+            return A.get(i).getValue();
+        else if (i == -1)
+            return "";
+        else
+            return A.get(-i-2).getValue();
+    }
+}
