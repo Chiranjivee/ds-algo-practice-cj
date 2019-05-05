3507e6d5c3d8af11876461dfc7d1b26627a0ab46
Chiranjivee <cjthehacker7@gmail.com>
2019-05-05 20:51:36 +0530

    LeetCode Problem 986 Interval List Intersection

---
+++ LeetCode/Problem_986_Interval_List_Intersection.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int[]> ans = new ArrayList();
+        int i = 0, j = 0;
+
+        while (i < A.length && j < B.length) {
+            // Let's check if A[i] intersects B[j].
+            // lo - the startpoint of the intersection
+            // hi - the endpoint of the intersection
+            int lo = Math.max(A[i][0], B[j][0]);
+            int hi = Math.min(A[i][1], B[j][1]);
+            if (lo <= hi)
+                ans.add(new int [] {lo, hi});
+
+            // Remove the interval with the smallest endpoint
+            if (A[i][1] < B[j][1])
+                i++;
+            else
+                j++;
+        }
+
+        
+        int [][] res= new int[ans.size()][];
+        int q = 0;
+        for (int[] e : ans) { 
+            res[q++] = e;
+        }
+        return res;
+    }
+}
