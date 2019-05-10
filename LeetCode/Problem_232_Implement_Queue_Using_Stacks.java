4019f25cfc1651048e02ba848fa88eb4068aad72
CJ <cjthehacker7@gmail.com>
2019-05-10 16:43:46 +0530

    Problem 232 Implement Queue Using Stacks

---
+++ LeetCode/Problem_232_Implement_Queue_Using_Stacks.java
@@ -0,0 +1,51 @@
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+class MyQueue {
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
+    int front;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        if (s1.isEmpty()) {
+            front = x;
+        }
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+        s1.push(x);
+        while(!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        int x = s1.pop();
+        if (!s1.empty())
+            front = s1.peek();
+        return x;
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return s1.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
