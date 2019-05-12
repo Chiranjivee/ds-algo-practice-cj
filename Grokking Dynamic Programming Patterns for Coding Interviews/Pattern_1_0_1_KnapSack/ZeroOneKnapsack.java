4428949d46b809fc8eb1e64a7b4d6e89f9a0ae3b
Chiranjivee <cjthehacker7@gmail.com>
2019-05-12 22:21:05 +0530

    Add Knapsack DP

--- Grokking Dynamic Programming Patterns for Coding Interviews/Pattern_1_0_1_KnapSack/ZeroOneKnapsack.java
+++ Grokking Dynamic Programming Patterns for Coding Interviews/Pattern_1_0_1_KnapSack/ZeroOneKnapsack.java
@@ -4,6 +4,35 @@ class ZeroOneKnapsack {
         return solveKnapsackUtil(weights, profits, totalBagWeight, 0);
     }
 
+    private int knapsackRecursive(
+        Integer[][] dp, 
+        int[] profits, 
+        int[] weights, 
+        int capacity,
+        int currentIndex) {
+
+    // base checks
+    if (capacity <= 0 || currentIndex >= profits.length)
+      return 0;
+
+    // if we have already solved a similar problem, return the result from memory
+    if(dp[currentIndex][capacity] != null)
+      return dp[currentIndex][capacity];
+
+    // recursive call after choosing the element at the currentIndex
+    // if the weight of the element at currentIndex exceeds the capacity, we shouldn't process this
+    int profit1 = 0;
+    if( weights[currentIndex] <= capacity )
+        profit1 = profits[currentIndex] + knapsackRecursive(dp, profits, weights,
+                capacity - weights[currentIndex], currentIndex + 1);
+
+    // recursive call after excluding the element at the currentIndex
+    int profit2 = knapsackRecursive(dp, profits, weights, capacity, currentIndex + 1);
+
+    dp[currentIndex][capacity] = Math.max(profit1, profit2);
+    return dp[currentIndex][capacity];
+  }
+
     public int solveKnapsackUtil(int []  weights, int [] profits, int n, int idx) {
         if (n <= 0 || idx >= profits.length) {
             return 0;
