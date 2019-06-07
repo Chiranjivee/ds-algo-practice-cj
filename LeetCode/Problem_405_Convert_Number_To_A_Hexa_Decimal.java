1df22e82008cba7eabd3e14410ed776d55c79cab
CJ <cjthehacker7@gmail.com>
2019-06-07 23:58:29 +0530

    Refactor 495

--- LeetCode/Problem_405_Convert_Number_To_A_Hexa_Decimal.java
+++ LeetCode/Problem_405_Convert_Number_To_A_Hexa_Decimal.java
@@ -1,11 +1,12 @@
 class Solution {
-    char[] map = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
-    
+    char[] map = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
     public String toHex(int num) {
-        if(num == 0) return "0";
+        if (num == 0)
+            return "0";
         String result = "";
-        while(num != 0){
-            result = map[(num & 15)] + result; 
+        while (num != 0) {
+            result = map[(num & 15)] + result;
             num = (num >>> 4);
         }
         return result;
