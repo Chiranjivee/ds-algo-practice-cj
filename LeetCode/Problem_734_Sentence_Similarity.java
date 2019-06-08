5745477ac7f5795fad6ee88fa4db5e7dc7f3d0ef
CJ <cjthehacker7@gmail.com>
2019-06-08 13:18:22 +0530

    LeetCode PRoblem 734 Sentence similarity

---
+++ LeetCode/Problem_734_Sentence_Similarity.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean areSentencesSimilar(String[] words1, String[] words2, List<List<String>> pairs) {
+        if (words1.length != words2.length) return false;
+
+        Set<String> pairset = new HashSet();
+        for (List<String> pair: pairs)
+            pairset.add(pair.get(0) + "#" + pair.get(1));
+
+        for (int i = 0; i < words1.length; ++i) {
+            if (!words1[i].equals(words2[i]) &&
+                    !pairset.contains(words1[i] + "#" + words2[i]) &&
+                    !pairset.contains(words2[i] + "#" + words1[i]))
+                return false;
+        }
+        return true;
+    }
+}
