2455bdb8f53710ef65ab2bd1adf22d74e3389cd5
CJ <cjthehacker7@gmail.com>
2019-06-09 23:50:34 +0530

    Problem 624 Maximum distance in arrays

---
+++ LeetCode/Problem_624_Maximum_Distance_In_Arrays.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxDistance(List<List<Integer>> arrays) {
+        
+        int result = Integer.MIN_VALUE;
+        int max = arrays.get(0).get(arrays.get(0).size() - 1);
+        int min = arrays.get(0).get(0);
+        
+        for (int i = 1; i < arrays.size(); i++) {
+            result = Math.max(result, Math.abs(arrays.get(i).get(0) - max));
+            result = Math.max(result, Math.abs(arrays.get(i).get(arrays.get(i).size() - 1) - min));
+            max = Math.max(max, arrays.get(i).get(arrays.get(i).size() - 1));
+            min = Math.min(min, arrays.get(i).get(0));
+        }
+        
+        return result;
+ 
+    }
+}
+
