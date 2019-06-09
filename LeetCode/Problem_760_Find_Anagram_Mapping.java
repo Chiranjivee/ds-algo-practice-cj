325ebe030297cbc068651ccd3c650dd4dfd12eb8
CJ <cjthehacker7@gmail.com>
2019-06-09 23:52:40 +0530

    Problem 760 Find anagram mapping

---
+++ LeetCode/Problem_760_Find_Anagram_Mapping.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] anagramMappings(int[] A, int[] B) {
+        Map<Integer, Integer> D = new HashMap();
+        for (int i = 0; i < B.length; ++i)
+            D.put(B[i], i);
+
+        int[] ans = new int[A.length];
+        int t = 0;
+        for (int x: A)
+            ans[t++] = D.get(x);
+        return ans;
+    }
+}
