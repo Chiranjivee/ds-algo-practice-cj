6f9fa3f96d6205ab9e0a09b0a5f39fc669383dce
CJ <cjthehacker7@gmail.com>
2019-06-09 23:55:08 +0530

    Problem 597 Friends Requst I Overall acceptance rate

---
+++ LeetCode/Problem_597_Friends_Request_I_Overall_Acceptance_Rate.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select
+round(
+    ifnull(
+    (select count(*) from (select distinct requester_id, accepter_id from request_accepted) as A)
+    /
+    (select count(*) from (select distinct sender_id, send_to_id from friend_request) as B),
+    0)
+, 2) as accept_rate;
