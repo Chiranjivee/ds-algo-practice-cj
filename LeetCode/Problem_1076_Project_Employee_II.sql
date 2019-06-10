05e2462ec0a2de7cdeb0fd00db6ecc215923b4f7
CJ <cjthehacker7@gmail.com>
2019-06-10 22:58:29 +0530

    LeetCode PRoblem 1076 Project Employee II

---
+++ LeetCode/Problem_1076_Project_Employee_II.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT project_id
+FROM project
+GROUP BY project_id
+HAVING COUNT(employee_id) =
+(
+    SELECT count(employee_id)
+    FROM project
+    GROUP BY project_id
+    ORDER BY count(employee_id) desc
+    LIMIT 1
+);
