d71eec9ec883314f51f181422bfc00a1f9b02d65
CJ <cjthehacker7@gmail.com>
2019-06-11 22:47:03 +0530

    LeetCode PRoblem 1056 Confusing number

---
+++ LeetCode/Problem_1056_Confusing_Number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean confusingNumber(int N) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(6, 9);
+        map.put(9, 6);
+        map.put(0, 0);
+        map.put(1, 1);
+        map.put(8, 8); 
+        int newNum = 0;
+        int x = N;
+        while (x != 0) {
+            int remainder = x % 10;
+            if (!map.containsKey(remainder)) 
+                return false;
+            newNum = newNum * 10 + map.get(remainder);
+            x /= 10;
+        }    
+        return N == newNum? false: true;        
+    }
+}
