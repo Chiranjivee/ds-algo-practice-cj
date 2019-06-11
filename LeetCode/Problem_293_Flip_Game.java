09cdc05d3b64690ced8205130e168d9d72109554
CJ <cjthehacker7@gmail.com>
2019-06-11 23:09:40 +0530

    LeetCode Problem 293 Flip Game

---
+++ LeetCode/Problem_293_Flip_Game.java
@@ -0,0 +1,8 @@
+class Solution {
+    public List<String> generatePossibleNextMoves(String s) {
+        List list = new ArrayList();
+        for (int i=-1; (i = s.indexOf("++", i+1)) >= 0; )
+            list.add(s.substring(0, i) + "--" + s.substring(i+2));
+        return list;
+    }
+}
