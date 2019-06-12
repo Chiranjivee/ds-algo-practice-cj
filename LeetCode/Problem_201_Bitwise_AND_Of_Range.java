c01f423b3cb78f301a62f1ef8cedef85bc5317ab
CJ <cjthehacker7@gmail.com>
2019-06-12 23:28:15 +0530

    Problem 201 Bitwise and of range

---
+++ LeetCode/Problem_201_Bitwise_AND_Of_Range.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        int step = 0;
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            step++;
+        }
+        return m << step;
+    }
+}
