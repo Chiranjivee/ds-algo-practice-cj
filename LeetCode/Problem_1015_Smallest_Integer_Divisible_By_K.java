aaa6f3349f53eefd18be9d7f5e174cf8b0b07b83
CJ <cjthehacker7@gmail.com>
2019-06-14 22:51:47 +0530

    LeetCode Problem 1015 Smallest Integer Divisible by K

---
+++ LeetCode/Problem_1015_Smallest_Integer_Divisible_By_K.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int smallestRepunitDivByK(int K) {
+        if (K % 2 == 0 || K % 5 == 0) return -1;
+        int r = 0;
+        for (int N = 1; N <= K; ++N) {
+            r = (r * 10 + 1) % K;
+            if (r == 0) return N;
+        }
+        return -1;
+    }
+}
