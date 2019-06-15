739b758512ad68e19cc3828e03aaf07ca13b7aee
CJ <cjthehacker7@gmail.com>
2019-06-15 12:47:26 +0530

    LeetCode Problem 1003 Check if word is valid after substitutions

---
+++ LeetCode/Problem_1003_Check_If_Word_Is_Valid_After_Substitutions.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isValid(String S) {
+        Stack<Character> stack = new Stack<>();
+        for (char c: S.toCharArray()) {
+            if (c == 'c') {
+                if (stack.isEmpty() || stack.pop() != 'b') return false;
+                if (stack.isEmpty() || stack.pop() != 'a') return false;
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
