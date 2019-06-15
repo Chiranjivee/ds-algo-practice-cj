92c13007040bbcaad7bfd0decf292d998edd9bde
CJ <cjthehacker7@gmail.com>
2019-06-15 13:11:53 +0530

    Problem 901 Online stock span

---
+++ LeetCode/Problem_901_Online_Stock_Span.java
@@ -0,0 +1,24 @@
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
+class StockSpanner {
+    Stack<Integer> prices, weights;
+    public StockSpanner() {
+        prices = new Stack<>();
+        weights = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int w = 1;
+        while (!prices.isEmpty() && prices.peek() <= price) {
+            prices.pop();
+            w += weights.pop();
+        }
+        
+        prices.push(price);
+        weights.push(w);
+        return w;
+    }
+}
