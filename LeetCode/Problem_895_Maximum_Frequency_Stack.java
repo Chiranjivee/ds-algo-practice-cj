16a77728fedfe7d29320f803bd03388ad2f1b6f6
CJ <cjthehacker7@gmail.com>
2019-06-15 13:26:40 +0530

    LeetCode Problem 895 Maximum Frequency Stack

---
+++ LeetCode/Problem_895_Maximum_Frequency_Stack.java
@@ -0,0 +1,35 @@
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ */
+class FreqStack {
+
+    Map<Integer, Integer> freq;
+    Map<Integer, Stack<Integer>> group;
+    int maxfreq;
+    
+    public FreqStack() {
+        freq = new HashMap();
+        group = new HashMap();
+        maxfreq = 0;
+    }
+    
+    public void push(int x) {
+        int f = freq.getOrDefault(x, 0) + 1;
+        freq.put(x, f);
+        if (f > maxfreq)
+            maxfreq = f;
+
+        group.computeIfAbsent(f, z-> new Stack()).push(x);
+    }
+    
+    public int pop() {
+        int x = group.get(maxfreq).pop();
+        freq.put(x, freq.get(x) - 1);
+        if (group.get(maxfreq).size() == 0)
+            maxfreq--;
+        return x;
+    }
+}
