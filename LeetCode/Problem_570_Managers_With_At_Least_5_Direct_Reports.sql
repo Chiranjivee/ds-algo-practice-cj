3ecf84ec4534ab69efe29fb5537861bbb0bd0365
CJ <cjthehacker7@gmail.com>
2019-06-19 22:42:39 +0530

    LeetCode Problem 570 Managers with at least 5 direct reports

---
+++ LeetCode/Problem_570_Managers_With_At_Least_5_Direct_Reports.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT
+    Name
+FROM
+    Employee AS t1 JOIN
+    (SELECT
+        ManagerId
+    FROM
+        Employee
+    GROUP BY ManagerId
+    HAVING COUNT(ManagerId) >= 5) AS t2
+    ON t1.Id = t2.ManagerId
+;
