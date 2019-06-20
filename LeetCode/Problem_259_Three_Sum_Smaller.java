e0ec8fe1ce81d581b59d592d1cc3176a76fcd718
CJ <cjthehacker7@gmail.com>
2019-06-20 17:09:19 +0530

    Problem 259 Three Sum smaller

---
+++ LeetCode/Problem_259_Three_Sum_Smaller.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            sum += twoSumSmaller(nums, i + 1, target - nums[i]);
+        }
+        return sum;
+    }
+
+    private int twoSumSmaller(int[] nums, int startIndex, int target) {
+        int sum = 0;
+        int left = startIndex;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] < target) {
+                sum += right - left;
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return sum;
+    }
+}
