469da2c3c3e64290ed8fa11acbb1b9c28ccd2e35
CJ <cjthehacker7@gmail.com>
2019-06-21 15:31:43 +0530

    Problem 280 Wiggle sort

---
+++ LeetCode/Problem_280_Wiggle_Sort.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if ((i % 2 == 0) == (nums[i] > nums[i + 1])) {
+                swap(nums, i, i + 1);
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
