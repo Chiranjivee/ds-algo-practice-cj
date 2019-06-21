1eb9282d35d3c49b826c9529a9c11067466a1583
CJ <cjthehacker7@gmail.com>
2019-06-21 14:36:45 +0530

    Problem 797 All paths from source to target

---
+++ LeetCode/Problem_797_All_Paths_From_Source_To_Target.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        return solve(graph, 0);
+    }
+
+    public List<List<Integer>> solve(int[][] graph, int node) {
+        int N = graph.length;
+        List<List<Integer>> ans = new ArrayList();
+        if (node == N - 1) {
+            List<Integer> path = new ArrayList();
+            path.add(N-1);
+            ans.add(path);
+            return ans;
+        }
+
+        for (int nei: graph[node]) {
+            for (List<Integer> path: solve(graph, nei)) {
+                path.add(0, node);
+                ans.add(path);
+            }
+        }
+        return ans;
+    }
+}
