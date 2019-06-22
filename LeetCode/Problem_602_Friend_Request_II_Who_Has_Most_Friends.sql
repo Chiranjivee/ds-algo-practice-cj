1724877cb595c7f25ce593f555a27b9b2a40e149
CJ <cjthehacker7@gmail.com>
2019-06-23 00:03:58 +0530

    Problem 602 Friend Request II Who has most friends

---
+++ LeetCode/Problem_602_Friend_Request_II_Who_Has_Most_Friends.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+select ids as id, cnt as num
+from
+(
+select ids, count(*) as cnt
+   from
+   (
+        select requester_id as ids from request_accepted
+        union all
+        select accepter_id from request_accepted
+    ) as tbl1
+   group by ids
+   ) as tbl2
+order by cnt desc
+limit 1
+;
