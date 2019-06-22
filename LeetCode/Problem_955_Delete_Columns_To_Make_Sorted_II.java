ba7108c0cfec248d3627084997ec7b89d28f8278
CJ <cjthehacker7@gmail.com>
2019-06-22 18:37:19 +0530

    Problem 955 Delete Columns to make sorted II

---
+++ LeetCode/Problem_955_Delete_Columns_To_Make_Sorted_II.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minDeletionSize(String[] A) {
+        int N = A.length;
+        int W = A[0].length();
+        int ans = 0;
+
+        // cur : all rows we have written
+        // For example, with A = ["abc","def","ghi"] we might have
+        // cur = ["ab", "de", "gh"].
+        String[] cur = new String[N];
+        for (int j = 0; j < W; ++j) {
+            // cur2 : What we potentially can write, including the
+            //        newest column col = [A[i][j] for i]
+            // Eg. if cur = ["ab","de","gh"] and col = ("c","f","i"),
+            // then cur2 = ["abc","def","ghi"].
+            String[] cur2 = Arrays.copyOf(cur, N);
+            for (int i = 0; i < N; ++i)
+                cur2[i] += A[i].charAt(j);
+
+            if (isSorted(cur2))
+                cur = cur2;
+            else
+                ans++;
+        }
+
+        return ans;
+    }
+    
+    public boolean isSorted(String[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i].compareTo(A[i+1]) > 0)
+                return false;
+
+        return true;
+    }
+}
