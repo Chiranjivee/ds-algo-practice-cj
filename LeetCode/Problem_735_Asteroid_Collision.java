2903f0705d6e4025ab745e4e60748251d533d601
CJ <cjthehacker7@gmail.com>
2019-06-24 23:25:56 +0530

    Problem 735 Asteroid Collision

---
+++ LeetCode/Problem_735_Asteroid_Collision.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack();
+        for (int ast: asteroids) {
+            collision: {
+                while (!stack.isEmpty() && ast < 0 && 0 < stack.peek()) {
+                    if (stack.peek() < -ast) {
+                        stack.pop();
+                        continue;
+                    } else if (stack.peek() == -ast) {
+                        stack.pop();
+                    }
+                    break collision;
+                }
+                stack.push(ast);
+            }
+        }
+
+        int[] ans = new int[stack.size()];
+        for (int t = ans.length - 1; t >= 0; --t) {
+            ans[t] = stack.pop();
+        }
+        return ans;
+    }
+}
