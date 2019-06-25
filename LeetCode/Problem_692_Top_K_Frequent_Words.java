b16df624d5f1c15bf30eefa56edb40e44d4be5d6
CJ <cjthehacker7@gmail.com>
2019-06-25 22:20:06 +0530

    Problem 692 Top K Frequent Words

---
+++ LeetCode/Problem_692_Top_K_Frequent_Words.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> count = new HashMap<>();
+        for (String word: words) {
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        }
+        
+        List<String> candidates = new ArrayList(count.keySet());
+        Collections.sort(candidates, (w1, w2) -> count.get(w1).equals(count.get(w2)) ?
+                w1.compareTo(w2) : count.get(w2) - count.get(w1));
+
+        return candidates.subList(0, k);
+
+    }
+}
