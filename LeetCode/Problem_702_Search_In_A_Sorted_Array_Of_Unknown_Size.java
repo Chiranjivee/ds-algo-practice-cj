fb4d11a959c09f17e8ce8d9c20f338ee3eb42936
CJ <cjthehacker7@gmail.com>
2019-06-27 17:13:13 +0530

    Problem 702 Search in a sorted array of unknown size

---
+++ LeetCode/Problem_702_Search_In_A_Sorted_Array_Of_Unknown_Size.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(ArrayReader reader, int target) {
+        int hi = 1;
+        while (reader.get(hi) < target) {
+            hi = hi << 1;
+        }
+        
+        int low = hi >> 1;
+        while (low <= hi) {
+            int mid = low+(hi-low)/2;
+            if (reader.get(mid) > target) {
+                hi = mid-1;
+            } else if (reader.get(mid) < target) {
+                low = mid+1;
+            } else {
+                return mid;
+            }
+        }
+        
+        return -1;
+    }
+}
