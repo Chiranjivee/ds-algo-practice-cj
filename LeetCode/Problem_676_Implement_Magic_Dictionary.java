9115f1298fad35a3c52cef2aa8f46fd64a597088
CJ <cjthehacker7@gmail.com>
2019-06-29 23:39:19 +0530

    LeetCode Problem 676 Implement Magic Dictionary

---
+++ LeetCode/Problem_676_Implement_Magic_Dictionary.java
@@ -0,0 +1,35 @@
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary obj = new MagicDictionary();
+ * obj.buildDict(dict);
+ * boolean param_2 = obj.search(word);
+ */
+class MagicDictionary {
+
+    Map<Integer, ArrayList<String>> buckets;
+    public MagicDictionary() {
+        buckets = new HashMap();
+    }
+
+    public void buildDict(String[] words) {
+        for (String word: words) {
+            buckets.computeIfAbsent(word.length(), x -> new ArrayList()).add(word);
+        }
+    }
+
+    public boolean search(String word) {
+        if (!buckets.containsKey(word.length())) return false;
+        for (String candidate: buckets.get(word.length())) {
+            int mismatch = 0;
+            for (int i = 0; i < word.length(); ++i) {
+                if (word.charAt(i) != candidate.charAt(i)) {
+                    if (++mismatch > 1) break;
+                }
+            }
+
+            if (mismatch == 1) return true;
+        }
+
+        return false;
+    }
+}
