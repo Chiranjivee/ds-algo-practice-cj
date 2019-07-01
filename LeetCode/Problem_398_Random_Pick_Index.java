add0d5925c87f24cb792388db22fb0d2700de132
CJ <cjthehacker7@gmail.com>
2019-07-01 22:07:08 +0530

    Problem 398 Random pick index

---
+++ LeetCode/Problem_398_Random_Pick_Index.java
@@ -0,0 +1,27 @@
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
+class Solution {
+    int[] nums;
+    Random rnd;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.rnd = new Random();
+    }
+    
+    public int pick(int target) {
+        int result = -1;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != target)
+                continue;
+            if (rnd.nextInt(++count) == 0)
+                result = i;
+        }
+        
+        return result;
+    }
+}
