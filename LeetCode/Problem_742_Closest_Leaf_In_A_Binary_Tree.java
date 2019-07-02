be358fb9719895208cd58002358b29b55142f5e7
CJ <cjthehacker7@gmail.com>
2019-07-02 20:26:58 +0530

    Problem 742 Closest leaf in a binary tree

---
+++ LeetCode/Problem_742_Closest_Leaf_In_A_Binary_Tree.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int findClosestLeaf(TreeNode root, int k) {
+        Map<TreeNode, List<TreeNode>> graph = new HashMap();
+        dfs(graph, root, null);
+
+        Queue<TreeNode> queue = new LinkedList();
+        Set<TreeNode> seen = new HashSet();
+
+        for (TreeNode node: graph.keySet()) {
+            if (node != null && node.val == k) {
+                queue.add(node);
+                seen.add(node);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                if (graph.get(node).size() <= 1)
+                    return node.val;
+                for (TreeNode nei: graph.get(node)) {
+                    if (!seen.contains(nei)) {
+                        seen.add(nei);
+                        queue.add(nei);
+                    }
+                }
+            }
+        }
+        throw null;
+    }
+
+    public void dfs(Map<TreeNode, List<TreeNode>> graph, TreeNode node, TreeNode parent) {
+        if (node != null) {
+            if (!graph.containsKey(node)) graph.put(node, new LinkedList<TreeNode>());
+            if (!graph.containsKey(parent)) graph.put(parent, new LinkedList<TreeNode>());
+            graph.get(node).add(parent);
+            graph.get(parent).add(node);
+            dfs(graph, node.left, node);
+            dfs(graph, node.right, node);
+        }
+    }
+}
