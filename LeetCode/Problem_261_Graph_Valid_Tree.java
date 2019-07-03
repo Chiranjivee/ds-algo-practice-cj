5857740305952f7096bb244cd9b8bdb1381cf823
CJ <cjthehacker7@gmail.com>
2019-07-03 13:21:49 +0530

    LeetCode Problem_261_Graph_Valid_Tree

---
+++ LeetCode/Problem_261_Graph_Valid_Tree.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        // initialize n isolated islands
+        int[] nums = new int[n];
+        Arrays.fill(nums, -1);
+        
+        // perform union find
+        for (int i = 0; i < edges.length; i++) {
+            int x = find(nums, edges[i][0]);
+            int y = find(nums, edges[i][1]);
+            
+            // if two vertices happen to be in the same set
+            // then there's a cycle
+            if (x == y) return false;
+            
+            // union
+            nums[x] = y;
+        }
+        
+        return edges.length == n - 1;
+    }
+    
+    int find(int nums[], int i) {
+        if (nums[i] == -1) return i;
+        return find(nums, nums[i]);
+    }
+}
