8784971697588321294e6eaa676335c5c252ee53
CJ <cjthehacker7@gmail.com>
2019-07-04 18:28:39 +0530

    LeetCode Problem 590 Find And Replace Pattern

---
+++ LeetCode/Problem_590_Find_And_Replace_Pattern.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> result = new ArrayList<>();
+        for (String word : words) {
+            if (matchPattern(word, pattern)) {
+                result.add(word);
+            }
+        }
+        
+        return result;
+    }
+    
+    private boolean matchPattern(String word, String pattern) {
+        if (word.length() != pattern.length()) {
+            return false;
+        }
+        
+        Map<Character, Character> m1 = new HashMap();
+        Map<Character, Character> m2 = new HashMap();
+
+        for (int i = 0; i < word.length(); ++i) {
+            char w = word.charAt(i);
+            char p = pattern.charAt(i);
+            if (!m1.containsKey(w)) m1.put(w, p);
+            if (!m2.containsKey(p)) m2.put(p, w);
+            if (m1.get(w) != p || m2.get(p) != w)
+                return false;
+        }
+
+        return true;
+        
+    }
+}
