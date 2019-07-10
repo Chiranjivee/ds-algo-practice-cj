415a6d6951ced0209bad6ac17c63d201a387234f
CJ <cjthehacker7@gmail.com>
2019-07-10 21:57:24 +0530

    LeetCode Problem 1112 Highest grade for each students

---
+++ LeetCode/Problem_1112_Highest_Grade_For_Each_Students.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+SELECT
+  student_id,
+  MIN(course_id) AS course_id,
+  grade
+FROM
+  Enrollments
+WHERE
+  (student_id, grade) IN (
+    SELECT
+      student_id,
+      MAX(grade)
+    FROM
+      Enrollments
+    GROUP BY
+      student_id)
+GROUP BY
+  student_id,
+  grade
+ORDER BY student_id;
