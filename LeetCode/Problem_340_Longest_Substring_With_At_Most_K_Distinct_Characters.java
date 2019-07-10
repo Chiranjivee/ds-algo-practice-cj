af783d299286da6e6e79957a3bece84e70f0aef9
CJ <cjthehacker7@gmail.com>
2019-07-10 21:55:18 +0530

    Problem 340 Longest substring with at most k distinct characters

---
+++ LeetCode/Problem_340_Longest_Substring_With_At_Most_K_Distinct_Characters.java
@@ -0,0 +1,33 @@
+class Solution {
+  public int lengthOfLongestSubstringKDistinct(String s, int k) {
+    int n = s.length();
+    if (n * k == 0)
+      return 0;
+
+    // sliding window left and right pointers
+    int left = 0;
+    int right = 0;
+    // hashmap character -> its rightmost position
+    // in the sliding window
+    HashMap<Character, Integer> hashmap = new HashMap<Character, Integer>();
+
+    int max_len = 1;
+
+    while (right < n) {
+      // add new character and move right pointer
+      hashmap.put(s.charAt(right), right++);
+
+      // slidewindow contains k characters
+      if (hashmap.size() == k + 1) {
+        // delete the leftmost character
+        int del_idx = Collections.min(hashmap.values());
+        hashmap.remove(s.charAt(del_idx));
+        // move left pointer of the slidewindow
+        left = del_idx + 1;
+      }
+
+      max_len = Math.max(max_len, right - left);
+    }
+    return max_len;
+  }
+}
