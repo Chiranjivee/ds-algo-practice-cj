9280348580355c36aa507842f12107fae7b410e7
CJ <cjthehacker7@gmail.com>
2019-07-10 22:56:52 +0530

    Problem 228 Summary Ranges

---
+++ LeetCode/Problem_228_Summary_Ranges.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> summary = new ArrayList<>();
+        for (int i = 0, j = 0; j < nums.length; ++j) {
+            // check if j + 1 extends the range [nums[i], nums[j]]
+            if (j + 1 < nums.length && nums[j + 1] == nums[j] + 1)
+                continue;
+            // put the range [nums[i], nums[j]] into the list
+            if (i == j)
+                summary.add(nums[i] + "");
+            else
+                summary.add(nums[i] + "->" + nums[j]);
+            i = j + 1;
+        }
+        return summary;
+    }
+}
