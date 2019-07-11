be4ea7f0bfedb2519bee0d87dbe70e678389d044
CJ <cjthehacker7@gmail.com>
2019-07-11 22:03:24 +0530

    LeetCode Problem 828 Unique Letter String

---
+++ LeetCode/Problem_828_Unique_Letter_String.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int uniqueLetterString(String S) {
+        Map<Character, List<Integer>> index = new HashMap();
+        for (int i = 0; i < S.length(); ++i) {
+            char c = S.charAt(i);
+            index.computeIfAbsent(c, x-> new ArrayList<Integer>()).add(i);
+        }
+
+        long ans = 0;
+        for (List<Integer> A: index.values()) {
+            for (int i = 0; i < A.size(); ++i) {
+                long prev = i > 0 ? A.get(i-1) : -1;
+                long next = i < A.size() - 1 ? A.get(i+1) : S.length();
+                ans += (A.get(i) - prev) * (next - A.get(i));
+            }
+        }
+
+        return (int) ans % 1_000_000_007;
+    }
+}
