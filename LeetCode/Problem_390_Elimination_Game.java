02e4bb0b2d118a4763aef86437bf9057861dd024
CJ <cjthehacker7@gmail.com>
2019-07-15 22:45:54 +0530

    LeetCode Problem 390 Elimination Game

---
+++ LeetCode/Problem_390_Elimination_Game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lastRemaining(int n) {
+        boolean left = true;
+        int remaining = n;
+        int step = 1;
+        int head = 1;
+        while (remaining > 1) {
+            if (left || remaining % 2 ==1) {
+                head = head + step;
+            }
+            remaining = remaining / 2;
+            step = step * 2;
+            left = !left;
+        }
+        return head;
+    }
+}
