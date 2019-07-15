90a71785e0d91ccfef00f101013698df54a403c8
CJ <cjthehacker7@gmail.com>
2019-07-15 22:02:29 +0530

    Problem 372 Super pow

---
+++ LeetCode/Problem_372_Super_Pow.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int superPow(int a, int[] b) {
+        if (a % 1337 == 0) return 0;
+        int p = 0;
+        for (int i : b) p = (p * 10 + i) % 1140;
+        if (p == 0) p += 1440;
+        return power(a, p, 1337);
+    }
+    public int power(int a, int n, int mod) {
+        a %= mod;
+        int ret = 1;
+        while (n != 0) {
+            if ((n & 1) != 0) ret = ret * a % mod;
+            a = a * a % mod;
+            n >>= 1;
+        }
+        return ret;
+    }
+}
