680baf6df7348c09ba1144bfba5569f8b55cc54b
CJ <cjthehacker7@gmail.com>
2019-07-15 21:46:11 +0530

    Problem 245 Shortest word distance

---
+++ LeetCode/Problem_245_Shortest_Word_Distance_III.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int shortestWordDistance(String[] words, String word1, String word2) {
+        int index = -1;
+            int min = words.length;
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].equals(word1) || words[i].equals(word2)) {
+                    if (index != -1 && (word1.equals(word2) || !words[index].equals(words[i]))) {
+                        min = Math.min(i - index, min);
+                    }
+                    index = i;
+                }
+            }
+            return min;
+    }
+}
