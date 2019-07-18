caef20b28582853bd8d918505d2f55d603432134
CJ <cjthehacker7@gmail.com>
2019-07-18 18:26:03 +0530

    LeetCode/Problem_1124_Longest_Well_Performing_Interval

---
+++ LeetCode/Problem_1124_Longest_Well_Performing_Interval.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestWPI(int[] hours) {
+        int res = 0, score = 0, n = hours.length;
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < n; ++i) {
+            score += hours[i] > 8 ? 1 : -1;
+            if (score > 0) {
+                res = i + 1;
+            } else {
+                seen.putIfAbsent(score, i);
+                if (seen.containsKey(score - 1))
+                    res = Math.max(res, i - seen.get(score - 1));
+            }
+        }
+        return res;
+    }
+}
