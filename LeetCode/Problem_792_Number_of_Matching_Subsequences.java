7e6639782d43ca5ada3e859181e23be8e6f12ed8
CJ <cjthehacker7@gmail.com>
2019-07-21 23:43:07 +0530

    LeetCode Problem 792 Number of matching subsequences

---
+++ LeetCode/Problem_792_Number_of_Matching_Subsequences.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int numMatchingSubseq(String S, String[] words) {
+        int ans = 0;
+        ArrayList<Node>[] heads = new ArrayList[26];
+        for (int i = 0; i < 26; ++i)
+            heads[i] = new ArrayList<Node>();
+
+        for (String word: words)
+            heads[word.charAt(0) - 'a'].add(new Node(word, 0));
+
+        for (char c: S.toCharArray()) {
+            ArrayList<Node> old_bucket = heads[c - 'a'];
+            heads[c - 'a'] = new ArrayList<Node>();
+
+            for (Node node: old_bucket) {
+                node.index++;
+                if (node.index == node.word.length()) {
+                    ans++;
+                } else {
+                    heads[node.word.charAt(node.index) - 'a'].add(node);
+                }
+            }
+            old_bucket.clear();
+        }
+        return ans;
+    }
+
+}
+
+class Node {
+    String word;
+    int index;
+    public Node(String w, int i) {
+        word = w;
+        index = i;
+    }
+}
