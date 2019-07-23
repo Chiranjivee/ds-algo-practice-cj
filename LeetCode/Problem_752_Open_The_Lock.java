c9d66cc408ab6f775d7556e024e1e45b3cb13b1f
CJ <cjthehacker7@gmail.com>
2019-07-23 22:11:43 +0530

    LeetCode Problem 752 Open the lock

---
+++ LeetCode/Problem_752_Open_The_Lock.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Set<String> dead = new HashSet();
+        for (String d: deadends) dead.add(d);
+
+        Queue<String> queue = new LinkedList();
+        queue.offer("0000");
+        queue.offer(null);
+
+        Set<String> seen = new HashSet();
+        seen.add("0000");
+
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            String node = queue.poll();
+            if (node == null) {
+                depth++;
+                if (queue.peek() != null)
+                    queue.offer(null);
+            } else if (node.equals(target)) {
+                return depth;
+            } else if (!dead.contains(node)) {
+                for (int i = 0; i < 4; ++i) {
+                    for (int d = -1; d <= 1; d += 2) {
+                        int y = ((node.charAt(i) - '0') + d + 10) % 10;
+                        String nei = node.substring(0, i) + ("" + y) + node.substring(i+1);
+                        if (!seen.contains(nei)) {
+                            seen.add(nei);
+                            queue.offer(nei);
+                        }
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
