fb815e10a1e2f85ab9ba5cf78bae14423822b738
CJ <cjthehacker7@gmail.com>
2019-07-27 00:00:14 +0530

    LeetCode Problem 158 Read N Characters given Read4 II Call multiple times

---
+++ LeetCode/Problem_158_Read_N_Characters_Given_Read4_II_Call_Multiple_times.java
@@ -0,0 +1,28 @@
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char[] buf); 
+ */
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    private int buffPtr = 0;
+    private int buffCnt = 0;
+    private char[] buff = new char[4];
+    public int read(char[] buf, int n) {
+        int ptr = 0;
+        while (ptr < n) {
+            if (buffPtr == 0) {
+                buffCnt = read4(buff);
+            }
+            if (buffCnt == 0) break;
+            while (ptr < n && buffPtr < buffCnt) {
+                buf[ptr++] = buff[buffPtr++];
+            }
+            if (buffPtr >= buffCnt) buffPtr = 0;
+        }
+        return ptr;
+    }
+}
