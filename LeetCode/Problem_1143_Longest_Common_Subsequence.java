9f210b5842d1b3f2e9bb2fc39287ab4f2d3e77f1
CJ <cjthehacker7@gmail.com>
2019-08-02 21:08:15 +0530

    LeetCode Problem 1143 Longest common subsequence

---
+++ LeetCode/Problem_1143_Longest_Common_Subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int [][] dp = new int[text1.length() + 1][text2.length() + 1];
+
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                char a = text1.charAt(i - 1);
+                char b = text2.charAt(j - 1);
+                
+                if (a == b) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[text1.length()][text2.length()];
+    }
+}
