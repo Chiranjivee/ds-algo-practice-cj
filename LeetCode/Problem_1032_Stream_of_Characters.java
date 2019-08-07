fe76aa3a0cd04237041b2cb87e9937b8b24883aa
CJ <cjthehacker7@gmail.com>
2019-08-07 21:20:11 +0530

    LeetCode Problem 1032 Stream of characters

---
+++ LeetCode/Problem_1032_Stream_of_Characters.java
@@ -0,0 +1,46 @@
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker obj = new StreamChecker(words);
+ * boolean param_1 = obj.query(letter);
+ */
+class StreamChecker {
+    class TrieNode {
+        boolean isWord;
+        TrieNode[] next = new TrieNode[26];
+    }
+
+    TrieNode root = new TrieNode();
+    StringBuilder sb = new StringBuilder();
+
+    public StreamChecker(String[] words) {
+        createTrie(words);
+    }
+
+    public boolean query(char letter) {
+        sb.append(letter);
+        TrieNode node = root;
+        for (int i = sb.length() - 1; i >= 0 && node != null; i--) {
+            char c = sb.charAt(i);
+            node = node.next[c - 'a'];
+            if (node != null && node.isWord) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void createTrie(String[] words) {
+        for (String s : words) {
+            TrieNode node = root;
+            int len = s.length();
+            for (int i = len - 1; i >= 0; i--) {
+                char c = s.charAt(i);
+                if (node.next[c - 'a'] == null) {
+                    node.next[c - 'a'] = new TrieNode();
+                }
+                node = node.next[c - 'a'];
+            }
+            node.isWord = true;
+        }
+    }
+}
