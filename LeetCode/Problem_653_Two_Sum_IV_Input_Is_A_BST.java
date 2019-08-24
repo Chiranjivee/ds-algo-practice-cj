8d605d014f14ca7063fff562ed247096ee759c96
CJ <cjthehacker7@gmail.com>
2019-08-24 22:15:41 +0530

    Refactor

--- LeetCode/Problem_653_Two_Sum_IV_Input_Is_A_BST.java
+++ LeetCode/Problem_653_Two_Sum_IV_Input_Is_A_BST.java
@@ -13,8 +13,8 @@ class Solution {
         List<Integer> nums = new ArrayList<>();
         inorder(root, nums);
         for(int i = 0, j = nums.size()-1; i<j;){
-            if(nums.get(i) + nums.get(j) == k)return true;
-            if(nums.get(i) + nums.get(j) < k)i++;
+            if(nums.get(i) + nums.get(j) == k) return true;
+            if(nums.get(i) + nums.get(j) < k) i++;
             else j--;
         }
         return false;
