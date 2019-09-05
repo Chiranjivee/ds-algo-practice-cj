fbeec6bab488637cd730cadc72d6b169b14bb6fe
CJ <cjthehacker7@gmail.com>
2019-09-06 00:39:32 +0530

    LeetCode Problem 621 Task scheduler

--- LeetCode/Problem_621_Task_Scheduler.java
+++ LeetCode/Problem_621_Task_Scheduler.java
@@ -1,10 +1,12 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] map = new int[26];
-        for (char c: tasks)
+        for (char c: tasks) {
             map[c - 'A']++;
+        }
+            
         Arrays.sort(map);
-        
+
         int max_val = map[25] - 1, idle_slots = max_val * n;
         for (int i = 24; i >= 0 && map[i] > 0; i--) {
             idle_slots -= Math.min(map[i], max_val);
