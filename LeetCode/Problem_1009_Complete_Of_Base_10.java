c1ff9e8408f9a6a4a12ef9d3dd87c1da91a65961
CJ <cjthehacker7@gmail.com>
2019-10-03 10:36:20 +0530

    Fix formatting for problem 1009

--- LeetCode/Problem_1009_Complete_Of_Base_10.java
+++ LeetCode/Problem_1009_Complete_Of_Base_10.java
@@ -1,9 +1,12 @@
 class Solution {
     public int bitwiseComplement(int N) {
-        if (N == 0) return 1;
-        double digitsReq =  Math.log(N) / Math.log(2);
+        if (N == 0) {
+            return 1;
+        }
+
+        double digitsReq = Math.log(N) / Math.log(2);
         ++digitsReq;
-        int mask = (1 << (int)digitsReq) - 1;
+        int mask = (1 << (int) digitsReq) - 1;
         return mask - N;
     }
 }
