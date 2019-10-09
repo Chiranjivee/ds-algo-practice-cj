ec182ba347d9a1dd741dbcc3d168f2075b43c64a
CJ <cjthehacker7@gmail.com>
2019-10-09 23:45:02 +0530

    Refactor

--- LeetCode/Problem_861_Score_After_Flipping_Matrix.java
+++ LeetCode/Problem_861_Score_After_Flipping_Matrix.java
@@ -4,8 +4,10 @@ class Solution {
         int ans = 0;
         for (int c = 0; c < C; ++c) {
             int col = 0;
-            for (int r = 0; r < R; ++r)
+            for (int r = 0; r < R; ++r) {
                 col += A[r][c] ^ A[r][0];
+            }
+
             ans += Math.max(col, R - col) * (1 << (C-1-c));
         }
         return ans;
