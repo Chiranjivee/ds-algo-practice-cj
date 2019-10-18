b9d90cf2c24c595278705c63c3b2cbdd4b72835d
CJ <cjthehacker7@gmail.com>
2019-10-19 00:14:39 +0530

    Refactor

--- LeetCode/Problem_642_Design_Search_AutoComplete_System.java
+++ LeetCode/Problem_642_Design_Search_AutoComplete_System.java
@@ -7,13 +7,16 @@ public class AutocompleteSystem {
         String sentence;
         int times;
     }
+
     class Trie {
         int times;
         Trie[] branches = new Trie[27];
     }
+
     public int int_(char c) {
         return c == ' ' ? 26 : c - 'a';
     }
+
     public void insert(Trie t, String s, int times) {
         for (int i = 0; i < s.length(); i++) {
             if (t.branches[int_(s.charAt(i))] == null)
@@ -22,6 +25,7 @@ public class AutocompleteSystem {
         }
         t.times += times;
     }
+
     public List < Node > lookup(Trie t, String s) {
         List <Node> list = new ArrayList <> ();
         for (int i = 0; i < s.length(); i++) {
@@ -32,15 +36,21 @@ public class AutocompleteSystem {
         traverse(s, t, list);
         return list;
     }
+
     public void traverse(String s, Trie t, List <Node> list) {
-        if (t.times > 0)
+        if (t.times > 0) {
             list.add(new Node(s, t.times));
-        for (char i = 'a'; i <= 'z'; i++) {
-            if (t.branches[i - 'a'] != null)
-                traverse(s + i, t.branches[i - 'a'], list);
         }
-        if (t.branches[26] != null)
+
+        for (char i = 'a'; i <= 'z'; i++) {
+            if (t.branches[i - 'a'] != null) {
+                traverse(s + i, t.branches[i - 'a'], list);
+            }
+        }
+
+        if (t.branches[26] != null) {
             traverse(s + ' ', t.branches[26], list);
+        }
     }
 
     Trie root;
