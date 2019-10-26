2a7e64ef1a2ead22f58b0e1177b6c38249f9cf6a
CJ <cjthehacker7@gmail.com>
2019-10-26 09:43:58 +0530

    1153

--- LeetCode/Problem_1153_String_Transforms_Into_Another_String.java
+++ LeetCode/Problem_1153_String_Transforms_Into_Another_String.java
@@ -3,8 +3,10 @@ class Solution {
         if (s1.equals(s2)) return true;
         Map<Character, Character> dp = new HashMap<>();
         for (int i = 0; i < s1.length(); ++i) {
-            if (dp.getOrDefault(s1.charAt(i), s2.charAt(i)) != s2.charAt(i))
+            if (dp.getOrDefault(s1.charAt(i), s2.charAt(i)) != s2.charAt(i)) {
                 return false;
+            }
+                
             dp.put(s1.charAt(i), s2.charAt(i));
         }        
 
