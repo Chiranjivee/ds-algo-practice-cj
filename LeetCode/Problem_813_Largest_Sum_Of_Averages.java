f98ca50a38f009aaad5d5b1666d98771e2286b5d
CJ <cjthehacker7@gmail.com>
2019-11-11 19:03:19 +0530

    Refactor

--- LeetCode/Problem_813_Largest_Sum_Of_Averages.java
+++ LeetCode/Problem_813_Largest_Sum_Of_Averages.java
@@ -11,7 +11,7 @@ class Solution {
 
         for (int k = 0; k < K-1; ++k) {
             for (int i = 0; i < N; ++i) {
-                for (int j = i+1; j < N; ++j)
+                for (int j = i+1; j < N; ++j) 
                     dp[i] = Math.max(dp[i], (P[j]-P[i]) / (j-i) + dp[j]);
             }
         }
