3129191dc9ef47fbf2497088f72d14e71e756c4b
CJ <cjthehacker7@gmail.com>
2019-11-21 21:59:16 +0530

    Coin change naive

---
+++ GeeksForGeeks/DynamicProgramming/CoinChangeProblem.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ * You have been given a set of coins of different denominations. like {2, 3, 5}
+ * Calculate the total number of ways such that, we can form a sum of N, using these number of coins.
+ * 
+ */
+class CoinChangeProblem {
+
+    public static int countWaysNaive(int [] denominations, int n) {
+        return countWaysNaiveRecursive(denominations, 0, n);
+    }
+
+    public static int countWaysNaiveRecursive(int [] denominations, int idx, int N) {
+
+        if (N == 0) {
+            return 1;
+        }
+
+        if (N < 0) {
+            return 0;
+        }
+
+        if (idx >= denominations.length && n >= 1) {
+            return 0;
+        }
+
+        return 
+            countWaysNaiveRecursive(denominations, idx + 1, N)
+                + countWaysNaiveRecursive(denominations, idx, N - denominations[idx]);
+    }
+
+    public static void main(String[] args) {
+        int [] denominations = {2, 3, 5};
+        int N = 15;
+        System.out.println(countWaysNaive(denominations, N));
+    }
+}
