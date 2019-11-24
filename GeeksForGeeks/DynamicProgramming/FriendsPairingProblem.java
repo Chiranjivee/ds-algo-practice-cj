5a23c5092df8be8ea11328274bab935104d29fa6
CJ <cjthehacker7@gmail.com>
2019-11-24 22:55:03 +0530

    Friends pairing problem with clairity

--- GeeksForGeeks/DynamicProgramming/FriendsPairingProblem.java
+++ GeeksForGeeks/DynamicProgramming/FriendsPairingProblem.java
@@ -17,8 +17,20 @@ class FriendsPairingProblem {
         return 
             getWaysToPairFriend(n - 1) +                 // The person chooses to stay alone,
                 (n - 1) * getWaysToPairFriend(n - 2);    // The person chooes to pair with any of the n - 1 people in the group, which could have n - 1 possibilites and the problem reduces to n - 2
-
     }
+
+    public int getWaysToPairFriend(int n) {
+        
+        int [] dp = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            if (i <= 2) dp[i] = i;
+            dp[i] = dp[i - 1] + (n - 1) * dp[n - 2];
+        }
+
+        return dp[n];
+    }
+
 
     public static void main(String [] args) {
         System.out.pritnln(FriendsPairingProblem.solve());
