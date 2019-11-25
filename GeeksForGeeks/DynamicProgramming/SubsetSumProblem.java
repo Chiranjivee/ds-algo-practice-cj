962ebda7da093780c5cf5f539feabb7b76c49156
CJ <cjthehacker7@gmail.com>
2019-11-26 00:20:38 +0530

    Subset problem

--- GeeksForGeeks/DynamicProgramming/SubsetSumProblem.java
+++ GeeksForGeeks/DynamicProgramming/SubsetSumProblem.java
@@ -24,6 +24,39 @@ class SubsetProblem {
             || isSubSetSum(set, idx + 1, target);
     }
 
+    public boolean isSubSetSumMemoised(int [] set, int target) {
+
+        int [][] dp = new int [target + 1][set.length];
+        for (int i = 0; i < target + 1; i ++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return isSubSetSum(set, 0, target, dp) == 1;
+    }
+
+    public int isSubSetSum(int [] set, int idx, int target, int [][] dp) {
+
+        if (dp[target][idx] != -1) {
+            return dp[target][idx];
+        }
+
+        if (target == 0) {
+            return dp[target][idx] = 1;
+        }
+
+        if (idx == set.length && target != 0) {
+            return dp[target][idx] = 0;
+        }
+
+        return 
+            dp[target][idx] = 
+                Math.max(
+                    isSubSetSum(set, idx + 1, target - set[idx]),
+                    isSubSetSum(set, idx + 1, target));
+    }
+
+
+
     public static void main(String[] args) {
         SubsetProblem subsetProblem = new SubsetProblem();
 
