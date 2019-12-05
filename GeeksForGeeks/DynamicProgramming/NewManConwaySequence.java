dc457f3060a263716b829d802ad6851764b1bcff
CJ <cjthehacker7@gmail.com>
2019-12-05 19:04:45 +0530

    Print n terms of newman conway seq

--- GeeksForGeeks/DynamicProgramming/NewManConwaySequence.java
+++ GeeksForGeeks/DynamicProgramming/NewManConwaySequence.java
@@ -21,6 +21,21 @@ class NewManConwaySequence {
         return f[n];
     }
 
+    static void printNtermsOfSequence(int n) {
+        int f[] = new int[n + 1];
+        f[0] = 0;
+        f[1] = 1;
+        f[2] = 1;
+
+        int i;
+
+        for (i = 3; i <= n; i++) {
+            f[i] = f[f[i - 1]] + f[i - f[i - 1]];
+        }
+
+        System.out.println(Arrays.toString(f));
+    }
+
     public static void main(String[] args) {
          int n = 10;
          System.out.println(sequence(n));
