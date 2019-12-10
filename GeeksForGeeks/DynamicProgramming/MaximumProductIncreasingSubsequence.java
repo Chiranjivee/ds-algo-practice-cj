32151839b6791d9811fa11d6cc9cbd916701c3ee
CJ <cjthehacker7@gmail.com>
2019-12-10 21:04:45 +0530

    Maximum Product increasing sub sequence

---
+++ GeeksForGeeks/DynamicProgramming/MaximumProductIncreasingSubsequence.java
@@ -0,0 +1,27 @@
+class MaximumProductIncreasingSubsequence {
+    public int getMaximumProductIncreasingSubsequence(int [] arr) {
+
+        int [] MSBSF = new int[arr.length + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            MSBSF[i] = arr[i];
+        }
+        
+        // calculate lis from left
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && MSBSF[i] < MSBSF[j] * arr[i]) {
+                    MSBSF[i] = MSBSF[j] * arr[j];
+                }
+            }
+        }
+
+        // calculate the max lis.
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, MSBSF[i]);
+        }
+
+        return max;
+    }
+}
