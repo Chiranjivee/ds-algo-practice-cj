13518ce6c55e466148b6f5426f909b01deb18aad
CJ <cjthehacker7@gmail.com>
2019-12-13 23:49:48 +0530

    Hello world

---
+++ Practice/HelloWorld.java
@@ -0,0 +1,6 @@
+class HelloWorld {
+    public static void main(String [] args) {
+        System.out.println("Couldn't do anything today, this day was very sad. Plus my father got a heart attack
+        I am shit scared. I don't know what should I do.!");
+    }
+}
