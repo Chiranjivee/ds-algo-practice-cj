d24943dd32e48c2ff5bbf3a0ea044137ca4d8d69
CJ <cjthehacker7@gmail.com>
2019-12-14 23:22:09 +0530

    Hello world

--- Practice/HelloWorld.java
+++ Practice/HelloWorld.java
@@ -2,5 +2,7 @@ class HelloWorld {
     public static void main(String [] args) {
         System.out.println("Couldn't do anything today, this day was very sad. Plus my father got a heart attack
         I am shit scared. I don't know what should I do.!");
+
+        System.out.println("Today, things went way far, and got very bad.")
     }
 }
