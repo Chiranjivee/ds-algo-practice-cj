b114c0fe181eb9f6eb057bdff7c4fbabcca28150
CJ <cjthehacker7@gmail.com>
2019-12-15 22:33:51 +0530

    GOOGLE

--- Practice/HelloWorld.java
+++ Practice/HelloWorld.java
@@ -3,6 +3,8 @@ class HelloWorld {
         System.out.println("Couldn't do anything today, this day was very sad. Plus my father got a heart attack
         I am shit scared. I don't know what should I do.!");
 
-        System.out.println("Today, things went way far, and got very bad.")
+        System.out.println("Today, things went way far, and got very bad.");
+
+        System.out.println("Resetting study state: Target: GOOGLE.")
     }
 }
