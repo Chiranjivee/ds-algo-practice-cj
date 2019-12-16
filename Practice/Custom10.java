b3ce8d372d9cc57ea688b67fcf69bf59f1625441
CJ <cjthehacker7@gmail.com>
2019-12-16 22:27:57 +0530

    Java examples

---
+++ Practice/Custom10.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Custom10 {
+
+    public static void main(String [] args) {
+
+        printHelloXTimes(5);
+
+        System.out.println("       ");
+
+        printHelloXTimes(4);
+
+        System.out.println("   -------------    ");
+
+        printHelloXTimes(0);
+
+        System.out.println("   -------------    ");
+
+    }
+
+    private static void printHelloXTimes(int x) {
+        for (int i = 0; i < x; i++) {
+            System.out.println("Hello");
+
+            System.out.println("Helasdflo");
+            System.out.println("asdf");
+            System.out.println("asdfasdf");
+            System.out.println("asdfasdfasdf");
+        }
+    }
+
+}
