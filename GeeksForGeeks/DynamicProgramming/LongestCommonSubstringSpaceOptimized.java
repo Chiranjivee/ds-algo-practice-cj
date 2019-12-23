1448b075cc903023f49c7e86b71d1386b12dc149
CJ <cjthehacker7@gmail.com>
2019-12-23 23:41:29 +0530

    Longest common substring space optimized

--- GeeksForGeeks/DynamicProgramming/LongestCommonSubstringSpaceOptimized.java
+++ GeeksForGeeks/DynamicProgramming/LongestCommonSubstringSpaceOptimized.java
@@ -3,21 +3,21 @@ class LongestCommonSubstring {
         int m = s1.length();
         int n = s2.length();
 
-        int [][] dp = new int[m][n];
-
+        int [][] dp = new int[2][n];
+        int currentRow = 0;
         int res = Integer.MIN_VALUE;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                
                 if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
+                    dp[currentRow][j] = 0;
                 } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    dp[currentRow][j] = 1 + dp[1 - currentRow][j - 1];
                 } else {
-                    dp[i][j] = 0
+                    dp[currentRow][j] = 0
                 }
 
                 res = Math.max(res, dp[i][j]);
+                currentRow = 1 - currentRow;
             }
         }
 
