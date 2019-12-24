86d676a0d095d1c798453aed1f46b0770cc4403f
CJ <cjthehacker7@gmail.com>
2019-12-24 23:29:40 +0530

    Number of paths from top left to bottom right

---
+++ GeeksForGeeks/DynamicProgramming/NumberOfPathsFromTopLeftToBottomRight.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int getWays(int m, int n) {
+        int [][] dp = new int[m][n];
+
+        Arrays.fill(dp[m - 1], 1);
+        for (int i = 0; i < m; i++) {
+            dp[i][n - 1] = 1;
+        }
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+            }
+        }
+
+        return dp[0][0];
+    }
+}
