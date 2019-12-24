6b71fe48e8d8d044ad52fa0d688753c592804bee
CJ <cjthehacker7@gmail.com>
2019-12-24 23:40:34 +0530

    Smallest sum contiguous subarray

--- GeeksForGeeks/DynamicProgramming/LargestSumContiguousSubarray.java
+++ GeeksForGeeks/DynamicProgramming/LargestSumContiguousSubarray.java
@@ -16,4 +16,16 @@ public class Solution {
 
         return maxSoFar;
     }
+
+    public int getSmallestSumSubarray(int [] arr) {
+        int minEndingHere = Integer.MAX_VALUE;
+        int minSoFar = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            minEndingHere = Math.min(minEndingHere + arr[i], arr[i]);
+            minSoFar = Math.min(minSoFar, minEndingHere);
+        }
+
+        return minSoFar;
+    }
 }
