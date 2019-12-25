1c427b7622800d0132c132f765d5c2d571f86889
CJ <cjthehacker7@gmail.com>
2019-12-25 21:23:34 +0530

    Count ways to cover distance

---
+++ GeeksForGeeks/DynamicProgramming/CountWaysToCoverDistance.java
@@ -0,0 +1,15 @@
+class CountWaysToCoverDistance {
+    public int getWays(int n) {
+        int [] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        return dp[n];
+    }
+}
