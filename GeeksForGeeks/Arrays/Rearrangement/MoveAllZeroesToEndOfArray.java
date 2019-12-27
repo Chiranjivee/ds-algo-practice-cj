5f9fa4fb71f174c7656be96031f21f9daf0dc0a3
CJ <cjthehacker7@gmail.com>
2019-12-27 22:49:55 +0530

    Move all zeroes to end of array set (2)

--- GeeksForGeeks/Arrays/Rearrangement/MoveAllZeroesToEndOfArray.java
+++ GeeksForGeeks/Arrays/Rearrangement/MoveAllZeroesToEndOfArray.java
@@ -12,4 +12,20 @@ class MoveZeroesToEnd {
             arr[count++] = 0;
         }
     }
+
+    void pushZeroesToEndSet2(int [] arr) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                swap(arr, count++, i);
+            }
+        }
+    }
+
+    void swap(int [] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
