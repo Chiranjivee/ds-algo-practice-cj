5926511c06b8ebc4cdee38ce45e3c40bd1f0a80b
CJ <cjthehacker7@gmail.com>
2019-12-27 23:02:51 +0530

    Rearrange positive and negative number in on time and space

--- GeeksForGeeks/Arrays/Rearrangement/RearrangePositiveAndNegativeNumberInONTimeAndO1Space.java
+++ GeeksForGeeks/Arrays/Rearrangement/RearrangePositiveAndNegativeNumberInONTimeAndO1Space.java
@@ -11,6 +11,30 @@ class RearrangePositiveAndNegativeAlternatively {
         }
     }
 
+    public int rearrangePositiveAndNegativeAlternativelyWithOrderMaintain(int [] arr) {
+        // -ve at odd
+        // +ve at even
+        int outOfPlace = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (outOfPlace > -1) {
+                if (arr[i] >= 0 && arr[outOfPlace] < 0
+                    || arr[i] < 0 && arr[outOfPlace] >= 0) {
+
+                    rightRotate(arr, outOfPlace, i);
+                    if (index - outOfPlace > 2) {
+                        outOfPlace += 2;
+                    } else {
+                        outOfPlace = -1;
+                    }
+                }
+            }
+
+            else if (outOfPlace == -1 && arr[i] < 0 && (i % 2 == 1) || arr[i] > 0 && i % 2 == 0) {
+                outOfPlace = i;
+            }
+        }
+    }
+
     public void pivotAgainstZero(int [] arr) {
         int i = -1;
         int n = arr.length;
