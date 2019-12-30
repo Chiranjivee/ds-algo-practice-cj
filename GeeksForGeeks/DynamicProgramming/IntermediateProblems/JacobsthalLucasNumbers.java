9bad620b832e5196221c3a2fe79a71a03d4ee815
CJ <cjthehacker7@gmail.com>
2019-12-30 18:47:53 +0530

    Jacobsthal lucas numbers

---
+++ GeeksForGeeks/DynamicProgramming/IntermediateProblems/JacobsthalLucasNumbers.java
@@ -0,0 +1,16 @@
+class JacobsthalLucasNumbers {
+    public int getNthJacobsthalLucasNumbers(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        int [] dp = new int[n + 1];
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 2 * dp[i - 2]; 
+        }
+
+        return dp[n];
+    }
+}
