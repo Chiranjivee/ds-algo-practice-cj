d5267fefebc952cd4451be7de8dbfb115f0224b2
CJ <cjthehacker7@gmail.com>
2019-12-30 22:58:17 +0530

    Temple offerings

---
+++ GeeksForGeeks/DynamicProgramming/IntermediateProblems/TempleOfferings.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class TempleOfferings {
+    public int getMinOfferings(int [] arr) {
+
+        int [] dp = new int[arr.length];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                dp[i] = dp[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] > arr[i + 1] && dp[i] < dp[i + 1]) {
+                dp[i] = 1 + arr[i + 1];
+            }
+        }
+
+        System.out.println(Arrays.toString(dp));
+    }
+}
