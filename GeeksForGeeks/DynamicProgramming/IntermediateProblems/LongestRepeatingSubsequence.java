9ea9f587faadc6804a54c4819ca3369795c006c5
CJ <cjthehacker7@gmail.com>
2020-01-02 20:34:58 +0530

    Longest repeating subsequence

---
+++ GeeksForGeeks/DynamicProgramming/IntermediateProblems/LongestRepeatingSubsequence.java
@@ -0,0 +1,19 @@
+class LongestRepeatingSubsequence {
+    public int findLongestRepeatingSubsequence(String str1, String str2) {
+        int [][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (s1.charAt(i - 1) == s2.charAt(j - 1) && i != j) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
+    }
+}
