60584ab93754090f5ebc5a5c8d42086de9b29566
CJ <cjthehacker7@gmail.com>
2020-01-06 17:28:14 +0530

    Minimum cost to make two string identical

---
+++ GeeksForGeeks/DynamicProgramming/IntermediateProblems/MinimumCostToMakeTwoStringIdentical.java
@@ -0,0 +1,25 @@
+class MinimumCostToMakeTwoStringIdentical {
+
+    int minCostToMakeStringIdentical(String s1, String s2, int x, int y) {
+        int len = longestCommonSubsequence(s1, s2);
+        return x * (s1.length() - len) + y * (s2.length() - len);
+    }
+
+    int longestCommonSubsequence(String s1, String s2) {
+        int [][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
+    }
+}
