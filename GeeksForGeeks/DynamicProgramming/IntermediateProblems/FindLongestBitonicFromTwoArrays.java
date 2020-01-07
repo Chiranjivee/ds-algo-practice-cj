31ea5ac5d075ce2755009cea43da84b5fcda92b8
CJ <cjthehacker7@gmail.com>
2020-01-07 17:41:27 +0530

    Find longest bitonic from two arrays

---
+++ GeeksForGeeks/DynamicProgramming/IntermediateProblems/FindLongestBitonicFromTwoArrays.java
@@ -0,0 +1,28 @@
+class FindLongestBitonicFromTwoArrays {
+    public int getLisLength(int [] arr) {
+
+        int [] dp = new int[arr.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
+                    lis[i] = 1 + lis[j];
+                }
+            }
+        }
+
+        int res = dp[0];
+        for (int i = 0; i < arr.length; i++) {
+            res = Math.max(res, dp[i]);
+        }
+
+        return res;
+    }
+
+    public int solve(int [] arr, int [] arr2) {
+        int lis = getLisLength(arr);
+        int lds = getLisLength(reverse(arr2));
+        return lis + lds;
+    }
+}
