73b26f128be2fc18a9f6be863eb05f2e35441833
CJ <cjthehacker7@gmail.com>
2020-01-10 15:23:00 +0530

    Binary search

---
+++ GeeksForGeeks/DivideAndConquer/StandardAlgorithms/BinarySearch.java
@@ -0,0 +1,19 @@
+class BinarySearch {
+    public int search(int [] arr, int key) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] > key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
