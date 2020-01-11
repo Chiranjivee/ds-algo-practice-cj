0dc67bb047e84a08581858cb7beb91cc78810e34
CJ <cjthehacker7@gmail.com>
2020-01-11 23:34:16 +0530

    Minimum element in rotated sorted array

---
+++ GeeksForGeeks/DivideAndConquer/BinarySearchBased/MinimumElementInRotatedSortedArray.java
@@ -0,0 +1,24 @@
+class MinimumElementInRotatedSortedArray {
+    public int minElement(int [] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (mid > 0 && arr[mid] < arr[mid - 1]) {
+                return mid;
+            } else if (mid < end && arr[mid + 1] < arr[mid]) {
+                return mid + 1;
+            }
+
+            if (arr[mid] < arr[end]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return 0;
+    }
+}
