cd567ee2afcc29ee2d9dc3382e5cdee3b9ab49d8
CJ <cjthehacker7@gmail.com>
2020-01-20 23:29:18 +0530

    Insertion sort

---
+++ GeeksForGeeks/Sorting/InsertionSort.java
@@ -0,0 +1,17 @@
+class InsertionSort {
+    public void sort(int [] arr) {
+        if (arr.length == 0 || arr.length == 1) {
+            return
+        }
+
+        for (int i = 2; i < arr.length; i++) {
+            int element = arr[i];
+            int j = i - 1;
+            while (j >= 0 && element <= arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j] = element;
+        }
+    }
+}
