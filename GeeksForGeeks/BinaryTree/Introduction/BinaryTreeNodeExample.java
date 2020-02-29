5e75cf77cce54b4af56d52cd4c5d88e64887fe5c
CJ <cjthehacker7@gmail.com>
2020-02-29 19:15:15 +0530

    Insertion in binary tree

--- GeeksForGeeks/BinaryTree/Introduction/BinaryTreeNodeExample.java
+++ GeeksForGeeks/BinaryTree/Introduction/BinaryTreeNodeExample.java
@@ -16,4 +16,25 @@ class BinaryTree  {
     public BinaryTree(int rootData) {
         this.root = new TreeNode(rootData);
     }
+    static void insert(TreeNode temp, int key) {
+        Queue<Node> q = new LinkedList<Node> ();
+        q.add(temp);
+
+        while (!q.isEmpty()) {
+            temp = q.peek();
+            q.remove();
+    
+            if (temp.left == null) {
+                temp.left = new TreeNode(key);
+                break;
+            } else
+                q.add(temp.left);
+    
+            if (temp.right == null) {
+                temp.right = new TreeNode(key);
+                break;
+            } else
+                q.add(temp.right);
+        }
+    }
 }
