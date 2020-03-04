22e7bed34f27ec1deefc34966791e443807528fc
CJ <cjthehacker7@gmail.com>
2020-03-04 21:28:02 +0530

    Binary tree to dll

--- GeeksForGeeks/BinaryTree/ConstructionAndConversion/BinaryTreeToDLL.java
+++ GeeksForGeeks/BinaryTree/ConstructionAndConversion/BinaryTreeToDLL.java
@@ -58,4 +58,25 @@ class Solution {
     
         return node;
     }
+
+    void BinaryTree2DoubleLinkedList(Node root) {
+        // Base case 
+        if (root == null)
+            return;
+    
+        // Recursively convert left subtree 
+        BinaryTree2DoubleLinkedList(root.left);
+    
+        // Now convert this node 
+        if (prev == null)
+            head = root;
+        else {
+            root.left = prev;
+            prev.right = root;
+        }
+        prev = root;
+    
+        // Finally convert right subtree 
+        BinaryTree2DoubleLinkedList(root.right);
+    }
 }
