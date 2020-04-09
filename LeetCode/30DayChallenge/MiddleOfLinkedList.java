a12959628d2dda98b74667658f3ac57b43c6849b
CJ <cjthehacker7@gmail.com>
2020-04-09 22:35:30 +0530

    Middle of linked list

---
+++ LeetCode/30DayChallenge/MiddleOfLinkedList.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return head;
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        if (fast == null) {
+            return slow;
+        }  else {
+            return slow.next;
+        }
+    }
+}
