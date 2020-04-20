80796b4760eefad7aea15bd829c0261710acd18f
CJ <cjthehacker7@gmail.com>
2020-04-20 22:22:56 +0530

    Number of islands

--- LeetCode/Practise/NumberOfIslands.java
+++ LeetCode/Practise/NumberOfIslands.java
@@ -14,11 +14,9 @@ class Solution {
     }
     
     void dfs(char[][] grid, int i, int j) {
-
         if ( i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
             return;
         }
-
         grid[i][j] = '0';
         dfs(grid, i + 1, j);
         dfs(grid, i, j + 1);
