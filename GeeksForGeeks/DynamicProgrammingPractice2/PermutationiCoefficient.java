7282b98e4441e8113682f9a95ba7d4bab2831130
CJ <cjthehacker7@gmail.com>
2020-04-29 23:47:07 +0530

    Permutation coefficient

---
+++ GeeksForGeeks/DynamicProgrammingPractice2/PermutationiCoefficient.java
@@ -0,0 +1,27 @@
+class Solution {
+    static int permutationCoeff(int n, int k) {
+        int P[][] = new int[n + 2][k + 2];
+
+        // Calculate value of Permutation  
+        // Coefficient in bottom up manner 
+        for (int i = 0; i<= n; i++) {
+            for (int j = 0; j<= Math.min(i, k); j++) {
+                // Base Cases 
+                if (j == 0)
+                    P[i][j] = 1;
+
+                // Calculate value using previosly  
+                // stored values 
+                else
+                    P[i][j] = P[i - 1][j] +
+                    (j * P[i - 1][j - 1]);
+
+                // This step is important  
+                // as P(i,j)=0 for j>i 
+                P[i][j + 1] = 0;
+            }
+        }
+
+        return P[n][k];
+    }
+}
