e7322c391f5179839648921cf47f664401cdb5cb
CJ <cjthehacker7@gmail.com>
2020-05-04 15:19:53 +0530

    Subset sum problem brute force

--- GeeksForGeeks/DynamicProgrammingPractice2/SubsetSumProblem.java
+++ GeeksForGeeks/DynamicProgrammingPractice2/SubsetSumProblem.java
@@ -27,4 +27,24 @@ class SubsetSumProblem {
 
         return subset[sum][n];
     }
+
+    static boolean isSubsetSum(int set[], int n, int sum) {
+        // Base Cases 
+        if (sum == 0)
+            return true;
+        if (n == 0 && sum != 0)
+            return false;
+
+        // If last element is greater than  
+        // sum, then ignore it 
+        if (set[n - 1] > sum)
+            return isSubsetSum(set, n - 1, sum);
+
+        /* else, check if sum can be obtained  
+        by any of the following 
+            (a) including the last element 
+            (b) excluding the last element */
+        return isSubsetSum(set, n - 1, sum) ||
+            isSubsetSum(set, n - 1, sum - set[n - 1]);
+    }
 }
