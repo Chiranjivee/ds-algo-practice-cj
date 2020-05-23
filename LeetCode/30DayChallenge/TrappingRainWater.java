c3c48dfa9b752e75e150a21f40a0ff7d9cf0b672
CJ <cjthehacker7@gmail.com>
2020-05-23 16:49:02 +0530

    Trapping rain water

---
+++ LeetCode/30DayChallenge/TrappingRainWater.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int trap(int[] height) {
+        
+        int left = 0;
+        int right = height.length - 1;
+        int currentHeight = 0;
+        int volume = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                currentHeight = Math.max(currentHeight, height[left]);
+                volume += currentHeight -  height[left];
+                left++;
+            } else {
+                currentHeight = Math.max(currentHeight, height[right]);
+                volume += currentHeight -  height[right];
+                right--;
+            }
+        }
+        
+        return volume;
+    }
+}
