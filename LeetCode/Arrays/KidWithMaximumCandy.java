590a7e9b37f1bbbcc2eb980ba2941fe7b12d9d59
CJ <cjthehacker7@gmail.com>
2020-05-25 19:33:23 +0530

    Kid with maximum candy

---
+++ LeetCode/Arrays/KidWithMaximumCandy.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> list = new ArrayList<>();
+        int max = 0;
+        for (int candy : candies) {
+            max = Math.max(candy, max);
+        }
+        
+        for (int candy: candies) {
+            if (candy + extraCandies >= max) {
+                list.add(true);
+            } else {
+                list.add(false);
+            }
+        }
+        
+        return list;
+    }
+}
