85105f889fd47ee436aeac2f2426b6d1b3bd5a88
CJ <cjthehacker7@gmail.com>
2020-06-03 23:31:41 +0530

    Reverse Linked List

---
+++ LeetCode/JuneChallenge/ReverseLinkedList.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode temp = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return temp;
+    }
+}
