8a21e776437971b3ec84869232a33916e7e31f8c
CJ <cjthehacker7@gmail.com>
2020-06-10 21:45:50 +0530

    Longest substring

---
+++ LeetCode/JuneChallenge/Longest_SubStrint.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public static void main(String[] args) {
+       System.out.println(lengthOfLongestSubstring("ABCAAD");
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        int ans = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0, j = 0; j < s.length(); j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+}
