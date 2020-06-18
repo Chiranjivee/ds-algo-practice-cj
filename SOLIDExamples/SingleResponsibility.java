62ef4e36345e3a386ec68c7f58a6b191b450352f
CJ <cjthehacker7@gmail.com>
2020-06-18 15:02:57 +0530

    Add SRP examples

--- SOLIDExamples/SingleResponsibility.java
+++ SOLIDExamples/SingleResponsibility.java
@@ -11,4 +11,57 @@ class Calculator {
             // error handlers.
         }
     }
+
+    public SubtractResult subtract(Number one, Number two) {
+        // subtract logic
+        return (SubtractResult) this.add(one, two.negate());
+    }
+}
+
+class Calculator {
+    public CalculationResult computeBinaryExpression(
+        Number one, 
+        Number two, 
+        OperationType op) {
+
+        return operationType.operate(one, two);
+    }
+
+    public CalculationResult computeUnaryExpression(
+        Number one, 
+        OperationType op) {
+
+        return operationType.operate(one);
+    }
+}
+
+// Just a name change for the class and the 
+// methods fall under one responsibility group
+class FileNetworkIOGateway {
+
+    public void uploadFile(File file, String url) {}
+
+    public File downloadFile(String url) {}
+}
+
+
+class MyResponsibleClass {
+
+    private DependencyOne dependencyOne;
+    private DependencyTwo dependencyTwo;
+    private DependencyThree dependencyThree;
+
+    public void myResponsibleMethod(Argument argument) {
+
+        Result1 resultFromFirstDependency = 
+            dependencyOne.callingItsResponsibleMethodFor(argument);
+
+        Result2 resultFromFirstDependency2 =
+            dependencyTwo.callingItsResponsibleMethodFor(resultFromFirstDependency);
+
+        Result3 resultFromFirstDependency3 =
+            dependencyThree.callingItsResponsibleMethodFor(resultFromFirstDependency2);
+
+        return resultFromFirstDependency3;
+    }
 }
