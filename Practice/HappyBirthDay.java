5581e540585f7c664c2021942a7f788ef2bbece5
Akshay Kumar <52469325+akthecoders@users.noreply.github.com>
2020-12-04 06:24:08 +0530

    Happy Birthday To CJ

--- Practice/HappyBirthDay.java
+++ Practice/HappyBirthDay.java
@@ -1,5 +1,6 @@
 class HappyBirthDay {
     public static void main(String [] args) {
-        System.out.println("Happy birth day to me!");
+        System.out.println("Happy birth day to me! 4th Dec 2019");
+        System.out.println("Happy birth day to you bro 4th Dec 2020! Enjoy today bro");
     }
 }
